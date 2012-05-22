2f9c69e21a5256d81181f8de61a312abde013275
Steven Casagrande <scasagrande@galvant.ca>
2012-05-22 14:08:33 -0400

    Added (near empty class) Lakeshore370

--- python/instruments/__init__.py
+++ python/instruments/__init__.py
@@ -5,6 +5,7 @@ from .keithley195 import Keithley195
 from .keithley580 import Keithley580
 from .keithley2182 import Keithley2182
 from .lakeshore340 import Lakeshore340
+from .lakeshore370 import Lakeshore370
 from .lakeshore475 import Lakeshore475
 from .oxforditc503 import OxfordITC503
 from .picowattavs47 import PicowattAVS47
