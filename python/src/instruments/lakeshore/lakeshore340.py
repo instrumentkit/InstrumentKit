b8f1f9d66203a11914ca9f03ebb91155fad421b3
Steven Casagrande <scasagrande@galvant.ca>
2013-04-30 13:11:17 -0400

    Lakeshore 340 should now be working

--- python/src/instruments/lakeshore/lakeshore340.py
+++ python/src/instruments/lakeshore/lakeshore340.py
@@ -1,24 +1,47 @@
 #!/usr/bin/python
-# Filename: lakeshore340.py
+# -*- coding: utf-8 -*-
+##
+# lakeshore340.py: Driver for the Lakeshore 340 cryogenic temp controller.
+##
+# © 2013 Steven Casagrande (scasagrande@galvant.ca).
+#
+# This file is a part of the GPIBUSB adapter project.
+# Licensed under the AGPL version 3.
+##
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
 
-# Original author: Steven Casagrande (stevencasagrande@gmail.com)
-# 2012
+## FEATURES ####################################################################
 
-# This work is released under the Creative Commons Attribution-Sharealike 3.0 license.
-# See http://creativecommons.org/licenses/by-sa/3.0/ or the included license/LICENSE.TXT file for more information.
+from __future__ import division
 
-# Attribution requirements can be found in license/ATTRIBUTION.TXT
+## IMPORTS #####################################################################
 
-from instruments.abstract_instruments import Instrument
+from instruments.generic_scpi import SCPIInstrument
 
-class Lakeshore340(Instrument):
-    def __init__(self, port, address,timeout_length):
-        super(Lakeshore340, self).__init__(self,port,address,timeout_length)
+## CLASSES #####################################################################
+
+class Lakeshore340(SCPIInstrument):
         
-    def temperature(self,sensor):
+    def temperature(self, sensor):
         '''
+        Read temperature of specified sensor.
         
+        :param int sensor: Sensor number to query.
+        
+        :rtype: `float`
         '''
-        if not isinstance(sensor,str):
-            raise Exception('Sensor must be specified as a string.')
-        return float(self.query('KRDG?' + sensor))
+        if not isinstance(sensor, int):
+            raise TypeError('Sensor must be specified as an int.')
+        return float(self.query('KRDG?{}'.format(sensor)))
