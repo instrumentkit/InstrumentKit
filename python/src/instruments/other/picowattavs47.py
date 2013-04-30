be7aea8d29c510fd379de19831490351f5e9ec44
Steven Casagrande <scasagrande@galvant.ca>
2013-04-30 13:54:08 -0400

    Picowatt AVS 47 should now be working

--- python/src/instruments/other/picowattavs47.py
+++ python/src/instruments/other/picowattavs47.py
@@ -1,57 +1,86 @@
 #!/usr/bin/python
-# Filename: picowattavs47.py
+# -*- coding: utf-8 -*-
+##
+# picowattavs47.py: Driver for the Picowatt AVS 47 resistance bridge.
+##
+# © 2013 Steven Casagrande (scasagrande@galvant.ca).
+#
+# This file is a part of the GPIBUSB adapter project.
+# Licensed under the AGPL version 3.
+##
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+##
 
-# Original author: Steven Casagrande (stevencasagrande@gmail.com)
-# 2012
+## FEATURES ####################################################################
 
-# This work is released under the Creative Commons Attribution-Sharealike 3.0 license.
-# See http://creativecommons.org/licenses/by-sa/3.0/ or the included license/LICENSE.TXT file for more information.
+from __future__ import division
 
-# Attribution requirements can be found in license/ATTRIBUTION.TXT
+## IMPORTS #####################################################################
 
 from instruments.abstract_instruments import Instrument
 
+## CLASSES #####################################################################
+
 class PicowattAVS47(Instrument):
-    def __init__(self, port, address,timeout_length):
-        super(PicowattAVS47, self).__init__(self,port,address,timeout_length)
         
-    def remote(self,state = None):
+    def remote(self, state=None):
         '''
         Enable or disable the remote mode state.
         
-        Enabling the remote mode allows all settings to be changed by computer interface and locks-out the front panel.
+        Enabling the remote mode allows all settings to be changed by computer 
+        interface and locks-out the front panel.
         
-        If not state is specified, function queries instrument for the current remote mode state.
+        If not state is specified, function queries instrument for the current 
+        remote mode state.
         
         Return type is a string.
         
-        state: Remote mode state
-        state = {OFF|DISABLE|ON|ENABLE|0|1},string/integer
+        :param state: Remote mode state. One of {OFF|DISABLE|ON|ENABLE|0|1}
+        :type: `int` or `str`
+        
+        :rtype: `str`
         '''
         if state == None:
             return self.query('REM?')
         
-        if isinstance(state,str):
+        if isinstance(state, str):
             state = state.lower()
         
-        if state in ('off','0',0,'disable'):
-            self.write('REM 0')
-        elif state in ('on','1',1,'enable'):
-            self.write('REM 1')
+        if state in ('off', '0', 0, 'disable'):
+            self.sendcmd('REM 0')
+        elif state in ('on', '1', 1, 'enable'):
+            self.sendcmd('REM 1')
         else:
-            raise Exception('Remote state must be "off", "on", "disable", "enable", "0" or "1".')
+            raise ValueError('Remote state must be "off", "on", "disable", '
+                               '"enable", "0" or "1".')
         
-    def inputSource(self,source = None):
+    def input_source(self, source=None):
         '''
         Set the input source.
-            1) "GROUND": Connects the bridge input to ground. Recommended before changing other bridge settings.
+            1) "GROUND": Connects the bridge input to ground. Recommended 
+            before changing other bridge settings.
             2) "ACTUAL": Connects to the actual measurement.
-            3) "REFerence": Connects to the internal precision 100ohm resistor. Used for calibrating the scale factor.
+            3) "REFerence": Connects to the internal precision 100ohm 
+            resistor. Used for calibrating the scale factor.
             
-        If no source is specified, function queries instrument for current input source connection.
+        If no source is specified, function queries instrument for current 
+        input source connection.
         
-        source: Input source
-        source = {GROund|ACTUAL|REFerence|100ohm|1|2|3},string/integer
+        :param source: Input source {GROund|ACTUAL|REFerence|100ohm|1|2|3}
+        :type: `int` or `str`
+        
+        :rtype: `str`
         '''
         if source == None:
             return self.query('INP?')
@@ -59,85 +88,88 @@ class PicowattAVS47(Instrument):
         if isinstance(source,str):
             source = source.lower()
         
-        if source in ('ground','gro','0',0):
-            self.write('INP 0')
-        elif source in ('actual','1',1):
-            self.write('INP 1')
-        elif source in ('reference','ref','100ohm','2',2):
-            self.write('INP 2')
+        if source in ('ground', 'gro', '0', 0):
+            self.sendcmd('INP 0')
+        elif source in ('actual', '1', 1):
+            self.sendcmd('INP 1')
+        elif source in ('reference', 'ref', '100ohm', '2', 2):
+            self.sendcmd('INP 2')
         else:
-            raise Exception('Valid sources include "ground", "actual", "reference",0,1,2,"100ohm", and "ref".')
+            raise ValueError('Valid sources include "ground", "actual", '
+                               ' "reference",0,1,2,"100ohm", and "ref".')
         
-    def muxChannel(self,channel = None):
+    def mux_channel(self, channel=None):
         '''
         Set the multiplexer channel.
-        It is recommended that you ground the input before switching the multiplexer channel.
+        It is recommended that you ground the input before switching the 
+        multiplexer channel.
         
-        If no channel is specified, function queries instrument for the current multiplexer channel setting.
+        If no channel is specified, function queries instrument for the current 
+        multiplexer channel setting.
         
-        Return type is a string.
+        :param int channel: Multiplexer channel. One of <0..7>.
         
-        channel: Multiplexer channel
-        channel = <0..7>,integer
+        :rtype: `str`
         '''
         if channel == None:
             return self.query('MUX?')
         
-        if not isinstance(channel,int):
-            raise Exception('Multiplexer channel must be an integer.')
+        if not isinstance(channel, int):
+            raise TypeError('Multiplexer channel must be an integer.')
         
-        if channel < 0 or channel > 7:
-            raise Exception('Multiplexer channel must be between [0,7].')
+        if (channel < 0) or (channel > 7):
+            raise ValueError('Multiplexer channel must be between [0,7].')
         
-        self.write( 'MUX ' + str(channel) )
+        self.sendcmd('MUX {}'.format(channel))
         
-    def excitation(self,channel = None):
+    def excitation(self, channel=None):
         '''
         Set the excitation channel.
         
-        If no channel is specified, function queries instrument for the current excitation channel setting.
+        If no channel is specified, function queries instrument for the 
+        current excitation channel setting.
         
-        Return type is a string.
+        :param int channel: Excitation channel. One of <0..7>.
         
-        channel: Excitation channel
-        channel = <0..7>,integer
+        :rtype: `str`
         '''
         if channel == None:
             return self.query('EXC?')
         
-        if not isinstance(channel,int):
-            raise Exception('Excitation channel must be an integer.')
+        if not isinstance(channel, int):
+            raise TypeError('Excitation channel must be an integer.')
         
-        if channel < 0 or channel > 7:
-            raise Exception('Excitation channel must be between [0,7].')
+        if (channel < 0) or (channel > 7):
+            raise ValueError('Excitation channel must be between [0,7].')
         
-        self.write( 'EXC ' + str(channel) )
+        self.sendcmd('EXC {}'.format(channel))
         
-    def display(self,channel = None):
+    def display(self, channel=None):
         '''
         Set the channel that is displayed on the front panel.
         
-        If no channel is specified, function queries instrument for the current display channel setting.
+        If no channel is specified, function queries instrument for the 
+        current display channel setting.
         
-        Return type is a string.
+        :param int channel: Display channel. One of <0..7>.
         
-        channel: Display channel
-        channel = <0..7>,integer
+        :rtype: `str`
         '''
         if channel == None:
             return self.query('EXC?')
         
-        if not isinstance(channel,int):
-            raise Exception('Display channel must be an integer.')
+        if not isinstance(channel, int):
+            raise TypeError('Display channel must be an integer.')
         
-        if channel < 0 or channel > 7:
-            raise Exception('Display channel must be between [0,7].')
+        if (channel < 0) or (channel > 7):
+            raise ValueError('Display channel must be between [0,7].')
         
-        self.write( 'DIS ' + str(channel) )
+        self.sendcmd('DIS {}'.format(channel))
         
     def adc(self):
         '''
-        Enable the alarm flag, ensuring that the next measurement reading is up to date.
+        Enable the alarm flag, ensuring that the next measurement reading 
+        is up to date.
         
         Call this function before quering the resistance.
         '''
@@ -147,9 +179,10 @@ class PicowattAVS47(Instrument):
         '''
         Perform a resistance query.
         
-        This command requires you to first call the function adc().
+        This command requires you to first call the function 
+        `~Picowattavs47.adc`.
         
-        Return type is a float.        
+        :rtype: `float`        
         '''
         res = self.query('RES?')
         return float( res[4:len(res)] ) # Remove leading "RES "
