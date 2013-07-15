f17cb29f47a8f669599d33deddbacbf9f41f9e19
Steven Casagrande <scasagrande@galvant.ca>
2013-07-15 12:04:17 -0400

    Updated names, url, and packages in setup file

--- python/setup.py
+++ python/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='GPIB-USB Instrument Communication Library',
+    name='InstrumentKit Communication Library',
     version='1.0beta1',
-    url='https://github.com/Galvant/gpibusb-comm_code/',
+    url='https://github.com/Galvant/InstrumentKit/',
     author='Steven Casagrande and Chris Granade',
     author_email='scasagrande@galvant.ca',
     package_dir={'': 'src'},
@@ -11,6 +11,7 @@ setup(
         'instruments',
         'instruments.abstract_instruments',
         'instruments.agilent',
+        'instruments.hp',
         'instruments.generic_scpi',
         'instruments.keithley',
         'instruments.lakeshore',
