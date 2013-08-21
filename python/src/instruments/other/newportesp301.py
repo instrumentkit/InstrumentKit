c732073dd1c848a7f27dd6582bb9d33fe6afd586
Thomas Alexander <taalexander@mta.ca>
2013-08-21 11:42:33 -0400

    Removed Delay

--- python/src/instruments/other/newportesp301.py
+++ python/src/instruments/other/newportesp301.py
@@ -351,7 +351,7 @@ class NewportESP301(Instrument):
             self.sendcmd(raw_cmd)
             
         if errcheck:
-            sleep(2)
+            
             err_resp = self.query('TB?')
             
             code, timestamp, msg = err_resp.split(",")
