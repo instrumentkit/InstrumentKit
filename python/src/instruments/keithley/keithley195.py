14ba5bc17078b92ec6a37b2fc9c46a6020999bc4
Steven Casagrande <scasagrande@galvant.com>
2014-01-28 21:08:11 -0500

    Finished refactoring keithley 580

--- python/src/instruments/keithley/keithley195.py
+++ python/src/instruments/keithley/keithley195.py
@@ -3,7 +3,7 @@
 ##
 # keithley195.py: Driver for the Keithley 195 multimeter.
 ##
-# © 2013 Steven Casagrande (scasagrande@galvant.ca).
+# © 2013-2014 Steven Casagrande (scasagrande@galvant.ca).
 #
 # This file is a part of the InstrumentKit project.
 # Licensed under the AGPL version 3.
