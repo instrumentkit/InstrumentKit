4ed562823d31a648362ef24f665b289f58f03abe
Christopher Granade <cgranade@cgranade.com>
2014-02-15 19:27:58 -0500

    Took advantage of new newport/ pkg to separate out NewportError.

--- python/src/instruments/newport/__init__.py
+++ python/src/instruments/newport/__init__.py
@@ -1,3 +1,4 @@
+from instruments.newport.errors import NewportError
 from instruments.newport.newportesp301 import (
-    NewportESP301, NewportESP301Axis, NewportError, NewportESP301HomeSearchMode
+    NewportESP301, NewportESP301Axis, NewportESP301HomeSearchMode
 )
