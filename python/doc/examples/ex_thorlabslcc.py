e13bdcd172ffaf4e6886666a8c9860dd2f8ccc48
Steven Casagrande <scasagrande@galvant.ca>
2014-08-20 18:09:12 -0400

    Cleaned up thorlabs lcc25 in similar way to sc10

--- python/doc/examples/ex_thorlabslcc.py
+++ python/doc/examples/ex_thorlabslcc.py
@@ -2,9 +2,9 @@
 
 import instruments as ik
 lcc = ik.thorlabs.LCC25.open_serial('COM10', 115200,timeout=1)
+
 #put model in voltage1 setting:
-lcc.mode = int(ik.thorlabs.LCC25mode.voltage2)
-#another way to do this is:
-lcc.mode = 2
+lcc.mode = llc.Mode.voltage1
+
 print("The current frequency is: ",lcc.frequency)
 print("The current voltage is: ",lcc.voltage1)
--- python/src/instruments/thorlabs/lcc25.py
+++ python/src/instruments/thorlabs/lcc25.py
@@ -3,7 +3,7 @@
 ##
 # lcc25.py: class for the Thorlabs LCC25 Liquid Crystal Controller
 ##
-# © 2013 Steven Casagrande (scasagrande@galvant.ca).
+# © 2014 Steven Casagrande (scasagrande@galvant.ca).
 #
 # This file is a part of the InstrumentKit project.
 # Licensed under the AGPL version 3.
@@ -22,20 +22,23 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 ##
 # LCC25 Class contributed by Catherine Holloway
+#
 ## IMPORTS #####################################################################
-from instruments.abstract_instruments import Instrument
+
 import quantities as pq
 from flufl.enum import IntEnum
 
-class LCC25mode(IntEnum):
-    modulate = 0
-    voltage1 = 1
-    voltage2 = 2
+from instruments.abstract_instruments import Instrument
+from instruments.util_fns import assume_units
+
+## CLASSES #####################################################################
 
 class LCC25(Instrument):
     """
     The LCC25 is a controller for the thorlabs liquid crystal modules.
-    it can set two voltages and then oscillate between them at a specific repetition rate
+    it can set two voltages and then oscillate between them at a specific 
+    repetition rate.
+    
     The user manual can be found here:
     http://www.thorlabs.com/thorcat/18800/LCC25-Manual.pdf
     """
@@ -43,12 +46,257 @@ class LCC25(Instrument):
         super(LCC25, self).__init__(filelike)
         self.terminator = "\r"
         self.end_terminator = ">"
+        
+    ## ENUMS ##
+    
+    class Mode(IntEnum):
+        modulate = 0
+        voltage1 = 1
+        voltage2 = 2
+    
+    ## PROPERTIES ##
 
+    def name(self):
+        """
+        gets the name and version number of the device
+        """
+        response = self.check_command("*idn?")
+        if response is "CMD_NOT_DEFINED":
+            self.name()          
+        else:
+            return response
+
+    @property
+    def frequency(self):
+        """
+        Gets/sets the frequency at which the LCC oscillates between the 
+        two voltages.
+        
+        :units: As specified (if a `~quantities.Quantity`) or assumed to be
+            of units Hertz.
+        :type: `~quantities.Quantity`
+        """
+        response = self.check_command("freq?")
+        if not response is "CMD_NOT_DEFINED":
+            return float(response)*pq.Hz
+    @frequency.setter
+    def frequency(self, newval):
+        newval = assume_units(newval, pq.Hz).rescale(pq.Hz).magnitude
+        if newval < 5:
+            raise ValueError("Frequency is too low.")
+        if newval >150:
+            raise ValueError("Frequency is too high")
+        self.sendcmd("freq={}".format(newval))
+
+    @property
+    def mode(self):
+        """
+        Gets/sets the output mode of the LCC25
+        
+        :type: `LCC25.Mode`
+        """
+        response = self.check_command("mode?")
+        if not response is "CMD_NOT_DEFINED":
+            return LCC25.Mode[int(response)]
+    @mode.setter
+    def mode(self, newval):
+        if (newval.enum is not LCC25.Mode):
+            raise TypeError("Mode setting must be a `LCC25.Mode` value, "
+                "got {} instead.".format(type(newval)))
+        response = self.query("mode={}".format(newval.value))
+
+    @property
+    def enable(self):
+        """
+        Gets/sets the output enable status.
+        
+        If output enable is on (`True`), there is a voltage on the output.
+        
+        :type: `bool`
+        """
+        response = self.check_command("enable?")
+        if not response is "CMD_NOT_DEFINED":
+            return True if int(response) is 1 else False
+    @enable.setter
+    def enable(self, newval):
+        if not isinstance(newval, bool):
+            raise TypeError("LLC25 enable property must be specified with a "
+                            "boolean.")
+        self.sendcmd("enable={}".format(int(newval)))
+    
+    @property
+    def extern(self):
+        """
+        Gets/sets the use of the external TTL modulation.
+        
+        Value is `True` for external TTL modulation and `False` for internal
+        modulation.
+        
+        :type: `bool`
+        """
+        response = self.check_command("extern?")
+        if not response is "CMD_NOT_DEFINED":
+            return True if int(response) is 1 else False
+    @extern.setter
+    def extern(self, newval):
+        if not isinstance(newval, bool):
+            raise TypeError("LLC25 extern property must be specified with a "
+                            "boolean.")
+        self.sendcmd("extern={}".format(int(newval)))
+
+    @property
+    def remote(self):
+        """
+        Gets/sets front panel lockout status for remote instrument operation.
+        
+        Value is `False` for normal operation and `True` to lock out the front
+        panel buttons.
+        
+        :type: `bool`
+        """
+        response = self.check_command("remote?")
+        if not response is "CMD_NOT_DEFINED":
+            return True if int(response) is 1 else False
+    @remote.setter
+    def remote(self, newval):
+        if not isinstance(newval, bool):
+            raise TypeError("LLC25 remote property must be specified with a "
+                            "boolean.")
+        self.sendcmd("remote={}".format(int(newval)))
+
+    @property
+    def voltage1(self):
+        """
+        Gets/sets the voltage value for output 1.
+        
+        :units: As specified (if a `~quantities.Quantity`) or assumed to be
+            of units Volts.
+        :type: `~quantities.Quantity`
+        """
+        response = self.check_command("volt1?")
+        if not response is "CMD_NOT_DEFINED":
+            return float(response)*pq.V
+    @voltage1.setter
+    def voltage1(self, newval):
+        newval = assume_units(newval, pq.V).rescale(pq.V).magnitude
+        if newval < 0:
+            raise ValueError("Voltage is too low.")
+        if newval > 25:
+            raise ValueError("Voltage is too high")
+        self.sendcmd("volt1={}".format(newval))
+
+    @property
+    def voltage2(self):
+        """
+        Gets/sets the voltage value for output 2.
+        
+        :units: As specified (if a `~quantities.Quantity`) or assumed to be
+            of units Volts.
+        :type: `~quantities.Quantity`
+        """
+        response = self.check_command("volt2?")
+        if not response is "CMD_NOT_DEFINED":
+            return float(response)*pq.V
+    @voltage1.setter
+    def voltage2(self, newval):
+        newval = assume_units(newval, pq.V).rescale(pq.V).magnitude
+        if newval < 0:
+            raise ValueError("Voltage is too low.")
+        if newval > 25:
+            raise ValueError("Voltage is too high")
+        self.sendcmd("volt2={}".format(newval))
+
+    @property
+    def min_voltage(self):
+        """
+        Gets/sets the minimum voltage value for the test mode.
+        
+        :units: As specified (if a `~quantities.Quantity`) or assumed to be
+            of units Volts.
+        :type: `~quantities.Quantity`
+        """
+        response = self.check_command("min?")
+        if not response is "CMD_NOT_DEFINED":
+            return float(response)*pq.V
+    @min_voltage.setter
+    def min_voltage(self, newval):
+        newval = assume_units(newval, pq.V).rescale(pq.V).magnitude
+        if newval < 0:
+            raise ValueError("Voltage is too low.")
+        if newval > 25:
+            raise ValueError("Voltage is too high")
+        self.sendcmd("min={}".format(newval))
+
+    @property
+    def max_voltage(self):
+        """
+        Gets/sets the maximum voltage value for the test mode. If the maximum 
+        voltage is less than the minimum voltage, nothing happens.
+        
+        :units: As specified (if a `~quantities.Quantity`) or assumed to be
+            of units Volts.
+        :type: `~quantities.Quantity`
+        
+        """
+        response = self.check_command("max?")
+        if not response is "CMD_NOT_DEFINED":
+            return float(response)*pq.V
+    @max_voltage.setter
+    def max_voltage(self, newval):
+        newval = assume_units(newval, pq.V).rescale(pq.V).magnitude
+        if newval < 0:
+            raise ValueError("Voltage is too low.")
+        if newval > 25:
+            raise ValueError("Voltage is too high")
+        self.sendcmd("max={}".format(newval))
+
+    @property
+    def dwell(self):
+        """
+        Gets/sets the dwell time for voltages for the test mode.
+        
+        :units: As specified (if a `~quantities.Quantity`) or assumed to be
+            of units milliseconds.
+        :type: `~quantities.Quantity`
+        """
+        response = self.check_command("dwell?")
+        if not response is "CMD_NOT_DEFINED":
+            return float(response)*pq.ms
+    @dwell.setter
+    def dwell(self, newval):
+        newval = int(assume_units(newval, pq.ms).rescale(pq.ms).magnitude)
+        if newval < 0:
+            raise ValueError("Dwell time must be positive")
+        self.sendcmd("dwell={}".format(newval))
+
+    @property
+    def increment(self):
+        """
+        Gets/sets the voltage increment for voltages for the test mode.
+        
+        :units: As specified (if a `~quantities.Quantity`) or assumed to be
+            of units Volts.
+        :type: `~quantities.Quantity`
+        """
+        response = self.check_command("increment?")
+        if not response is "CMD_NOT_DEFINED":
+            return float(response)*pq.V
+    @increment.setter
+    def increment(self, newval):
+        newval = assume_units(newval, pq.V).rescale(pq.V).magnitude
+        if newval < 0:
+            raise ValueError("Increment voltage must be positive")
+        self.sendcmd("increment={}".format(newval))
+
+    ## METHODS ##
+    
     def check_command(self,command):
         """
-        Checks for the \"Command error CMD_NOT_DEFINED\" error, which can sometimes occur if there were
-        incorrect terminators on the previous command. If the command is successful, it returns the value,
-        if not, it returns CMD_NOT_DEFINED
+        Checks for the \"Command error CMD_NOT_DEFINED\" error, which can 
+        sometimes occur if there were incorrect terminators on the previous 
+        command. If the command is successful, it returns the value, if not, 
+        it returns CMD_NOT_DEFINED
+        
         check_command will also clear out the query string
         """
         response = self.query(command)
@@ -65,200 +313,14 @@ class LCC25(Instrument):
         else:
             output_str = "CMD_NOT_DEFINED"
         return output_str
-
-
-    def name(self):
-        """
-        gets the name and version number of the device
-        """
-        response = self.check_command("*idn?")
-        if response is "CMD_NOT_DEFINED":
-            self.name()          
-        else:
-            return response
-
-    ### LCC SETTINGS ###
-
-    @property
-    def frequency(self):
-        """
-        The frequency at which the LCC oscillates between the two voltages
-        """
-        response = self.check_command("freq?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)*pq.Hz
-    @frequency.setter
-    def frequency(self, newval):
-        if newval < 5:
-            raise ValueError("Frequency is too low.")
-        if newval >150:
-            raise ValueError("Frequency is too high")
-        self.sendcmd("freq={}".format(newval))
-
-    @property
-    def mode(self):
-        """
-        The output mode of the LCC
-        mode 0 modulates between voltage 1 and voltage 2
-        mode 1 sets the output to voltage 1
-        mode 2 sets the output to voltage 2
-        """
-        response = self.check_command("mode?")
-        if not response is "CMD_NOT_DEFINED":
-            return int(response)
-    @mode.setter
-    def mode(self, newval):
-        if newval != 0 and newval != 1 and newval !=2:
-            raise ValueError("Not a valid output mode")
-        response = self.query("mode={}".format(newval))
-        #print(response)
-
-    @property
-    def enable(self):
-        """
-        If output enable is on, there is a voltage on the output
-        """
-        response = self.check_command("enable?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)
-    @enable.setter
-    def enable(self, newval):
-        if newval != 0 and newval != 1:
-            raise ValueError("Not a valid value for output enable")
-        self.sendcmd("enable={}".format(newval))
     
-    @property
-    def extern(self):
-        """
-        0 for internal modulation, 1 for external TTL modulation
-        """
-        response = self.check_command("extern?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)
-    @extern.setter
-    def extern(self, newval):
-        if newval != 0 and newval != 1:
-            raise ValueError("Not a valid value for external modulation enable")
-        self.sendcmd("extern={}".format(newval))
-
-    @property
-    def remote(self):
-        """
-        0 for normal operation, 1 to lock out buttons
-        """
-        response = self.check_command("remote?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)
-    @remote.setter
-    def remote(self, newval):
-        if newval != 0 and newval != 1:
-            raise ValueError("Not a valid value for remote operation enable")
-        self.sendcmd("remote={}".format(newval))
-
-    @property
-    def voltage1(self):
-        """
-        The voltage value for output 1
-        """
-        response = self.check_command("volt1?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)*pq.V
-    @voltage1.setter
-    def voltage1(self, newval):
-        if newval < 0:
-            raise ValueError("Voltage is too low.")
-        if newval >25:
-            raise ValueError("Voltage is too high")
-        self.sendcmd("volt1={}".format(newval))
-
-    @property
-    def voltage2(self):
-        """
-        The voltage value for output 2
-        """
-        response = self.check_command("volt2?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)*pq.V
-    @voltage1.setter
-    def voltage2(self, newval):
-        if newval < 0:
-            raise ValueError("Voltage is too low.")
-        if newval >25:
-            raise ValueError("Voltage is too high")
-        self.sendcmd("volt2={}".format(newval))
-
-
-    @property
-    def min_voltage(self):
-        """
-        The minimum voltage value for the test mode
-        """
-        response = self.check_command("min?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)*pq.V
-    @min_voltage.setter
-    def min_voltage(self, newval):
-        if newval < 0:
-            raise ValueError("Voltage is too low.")
-        if newval >25:
-            raise ValueError("Voltage is too high")
-        self.sendcmd("min={}".format(newval))
-
-    
-    @property
-    def max_voltage(self):
-        """
-        The maximum voltage value for the test mode
-        if the maximum voltage is greater than the minimum voltage, nothing happens
-        """
-        response = self.check_command("max?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)*pq.V
-    
-    @max_voltage.setter
-    def max_voltage(self, newval):
-        if newval < 0:
-            raise ValueError("Voltage is too low.")
-        if newval >25:
-            raise ValueError("Voltage is too high")
-        self.sendcmd("max={}".format(newval))
-
-    @property
-    def dwell(self):
-        """
-        The dwell time for voltages for the test mode
-        """
-        response = self.check_command("dwell?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)*pq.ms
-    
-    @dwell.setter
-    def dwell(self, newval):
-        if newval < 0:
-            raise ValueError("Dwell time must be positive")
-        self.sendcmd("dwell={}".format(newval))
-
-    @property
-    def increment(self):
-        """
-        The voltage increment for voltages for the test mode
-        """
-        response = self.check_command("increment?")
-        if not response is "CMD_NOT_DEFINED":
-            return float(response)*pq.V
-    
-    @increment.setter
-    def increment(self, newval):
-        if newval < 0:
-            raise ValueError("Increment voltage must be positive")
-        self.sendcmd("increment={}".format(newval))
-
-
-    ### LCC Methods ###
     def default(self):
         """
-        restores factory settings
-        returns 1 if successful
+        Restores instrument to factory settings.
+        
+        Returns 1 if successful, 0 otherwise
+        
+        :rtype: `int`
         """
         response = self.check_command("default")
         if not response is "CMD_NOT_DEFINED":
@@ -268,8 +330,11 @@ class LCC25(Instrument):
 
     def save(self):
         """
-        stores the parameters in static memory
-        returns 1 if successful
+        Stores the parameters in static memory
+        
+        Returns 1 if successful, zero otherwise.
+        
+        :rtype: `int`
         """
         response = self.check_command("save")
         if not response is "CMD_NOT_DEFINED":
@@ -277,10 +342,16 @@ class LCC25(Instrument):
         else:
             return 0
 
-    def set_settings(self,slot):
+    def set_settings(self, slot):
         """
-        saves the current settings to memory
-        returns 1 if successful
+        Saves the current settings to memory
+        
+        Returns 1 if successful, zero otherwise.
+        
+        :param slot: Memory slot to use, valid range `[1,4]`
+        :type slot: `int`
+        
+        :rtype: `int`
         """
         if slot < 0:
             raise ValueError("Slot number is less than 0")
@@ -294,8 +365,14 @@ class LCC25(Instrument):
 
     def get_settings(self,slot):
         """
-        gets the current settings from memory
-        returns 1 if successful
+        Gets the current settings to memory
+        
+        Returns 1 if successful, zero otherwise.
+        
+        :param slot: Memory slot to use, valid range `[1,4]`
+        :type slot: `int`
+        
+        :rtype: `int`
         """
         if slot < 0:
             raise ValueError("Slot number is less than 0")
@@ -309,8 +386,13 @@ class LCC25(Instrument):
 
     def test_mode(self):
         """
-        Puts the LCC in test mode - meaning it will increment the output voltage from the minimum value to the maximum value, in increments, waiting for the dwell time
-        returns 1 if successful
+        Puts the LCC in test mode - meaning it will increment the output 
+        voltage from the minimum value to the maximum value, in increments, 
+        waiting for the dwell time
+        
+        Returns 1 if successful, zero otherwise.
+        
+        :rtype: `int`
         """
         response = self.check_command("test")
         if not response is "CMD_NOT_DEFINED":
