78c6f398c424dfd15c937526c5f954a6f7080834
Steven Casagrande <scasagrande@galvant.ca>
2014-08-20 13:31:40 -0400

    Fixed import errors from last commit

--- python/src/instruments/thorlabs/__init__.py
+++ python/src/instruments/thorlabs/__init__.py
@@ -3,4 +3,4 @@ from instruments.thorlabs.thorlabsapt import (
 )
 from instruments.thorlabs.pm100usb import PM100USB
 from instruments.thorlabs.lcc25 import LCC25, LCC25mode
-from instruments.thorlabs.sc10 import SC10, SC10mode
+from instruments.thorlabs.sc10 import SC10
