e3608370eacf23ba2f3304213e44509bbddf0830
Steven Casagrande <scasagrande@galvant.ca>
2014-09-03 10:33:36 -0400

    Changed year marker in header

--- python/src/instruments/tests/test_thorlabs/__init__.py
+++ python/src/instruments/tests/test_thorlabs/__init__.py
@@ -3,7 +3,7 @@
 ##
 # __init__.py: Tests for Thorlabs-brand instruments.
 ##
-# © 2013 Steven Casagrande (scasagrande@galvant.ca).
+# © 2014 Steven Casagrande (scasagrande@galvant.ca).
 #
 # This file is a part of the InstrumentKit project.
 # Licensed under the AGPL version 3.
