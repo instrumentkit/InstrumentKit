a8c40f87be8745ea057ec3bfb88ad97c8c72a19d
Steven Casagrande <scasagrande@galvant.ca>
2016-01-10 23:53:49 -0500

    Add vxi11 communicator tests

---
+++ instruments/instruments/tests/test_comm/test_vxi11.py
@@ -0,0 +1,155 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# test_util_fns.py: Tests various utility functions.
+#
+# © 2016 Steven Casagrande (scasagrande@galvant.ca).
+#
+# This file is a part of the InstrumentKit project.
+# Licensed under the AGPL version 3.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+# IMPORTS ####################################################################
+
+from nose.tools import raises, eq_
+import mock
+
+from instruments.abstract_instruments.comm import VXI11Communicator
+
+# TEST CASES #################################################################
+
+import_base = 'instruments.abstract_instruments.comm.vxi11_communicator.vxi11'
+
+
+@mock.patch(import_base)
+def test_vxi11comm_init(mock_vxi11):
+    comm = VXI11Communicator("host")
+    mock_vxi11.Instrument.assert_called_with("host")
+
+
+@mock.patch(import_base)
+def test_vxi11comm_address(mock_vxi11):
+    # Create our communicator
+    comm = VXI11Communicator()
+
+    # Add in the host and name properties which are usually
+    # done in vxi11.Instrument.__init__
+    host = mock.PropertyMock(return_value="host")
+    name = mock.PropertyMock(return_value="name")
+    type(comm._inst).host = host
+    type(comm._inst).name = name
+
+    # Check that our address function is working
+    eq_(comm.address, ["host", "name"])
+    host.assert_called_with()
+    name.assert_called_with()
+
+
+@mock.patch(import_base)
+def test_vxi11comm_terminator(mock_vxi11):
+    comm = VXI11Communicator()
+
+    term_char = mock.PropertyMock(return_value="\n")
+    type(comm._inst).term_char = term_char
+
+    eq_(comm.terminator, "\n")
+    term_char.assert_called_with()
+
+    comm.terminator = "*"
+    term_char.assert_called_with("*")
+
+
+@mock.patch(import_base)
+def test_vxi11comm_timeout(mock_vxi11):
+    comm = VXI11Communicator()
+
+    timeout = mock.PropertyMock(return_value=30)
+    type(comm._inst).timeout = timeout
+
+    eq_(comm.timeout, 30)
+    timeout.assert_called_with()
+
+    comm.timeout = 10
+    timeout.assert_called_with(10)
+
+
+@mock.patch(import_base)
+def test_vxi11comm_close(mock_vxi11):
+    comm = VXI11Communicator()
+
+    comm.close()
+    comm._inst.close.assert_called_with()
+
+
+@mock.patch(import_base)
+def test_vxi11comm_read(mock_vxi11):
+    comm = VXI11Communicator()
+    comm._inst.read.return_value = "mock"
+
+    eq_(comm.read(), "mock")
+    comm._inst.read.assert_called_with(num=-1)
+
+    comm.read(10)
+    comm._inst.read.assert_called_with(num=10)
+
+
+@mock.patch(import_base)
+def test_vxi11comm_write(mock_vxi11):
+    comm = VXI11Communicator()
+
+    comm.write("mock")
+    comm._inst.write.assert_called_with("mock")
+
+
+@mock.patch(import_base)
+def test_vxi11comm_sendcmd(mock_vxi11):
+    comm = VXI11Communicator()
+
+    comm._sendcmd("mock")
+    comm._inst.write.assert_called_with("mock")
+
+
+@mock.patch(import_base)
+def test_vxi11comm_query(mock_vxi11):
+    comm = VXI11Communicator()
+    comm._inst.ask.return_value = "answer"
+
+    eq_(comm._query("mock"), "answer")
+    comm._inst.ask.assert_called_with("mock", num=-1)
+
+    comm._query("mock", size=10)
+    comm._inst.ask.assert_called_with("mock", num=10)
+
+
+@raises(NotImplementedError)
+@mock.patch(import_base)
+def test_vxi11comm_seek(mock_vxi11):
+    comm = VXI11Communicator()
+    comm.seek(1)
+
+
+@raises(NotImplementedError)
+@mock.patch(import_base)
+def test_vxi11comm_tell(mock_vxi11):
+    comm = VXI11Communicator()
+    comm.tell()
+
+
+@raises(NotImplementedError)
+@mock.patch(import_base)
+def test_vxi11comm_flush(mock_vxi11):
+    comm = VXI11Communicator()
+    comm.flush()
