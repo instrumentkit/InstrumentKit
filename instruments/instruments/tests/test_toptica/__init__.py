b0131d808676dc06802a4c743e1f3d7d31ac5d56
Catherine <milankie@gmail.com>
2016-01-18 16:16:46 -0800

    fixed tests

--- instruments/instruments/tests/test_toptica/__init__.py
+++ instruments/instruments/tests/test_toptica/__init__.py
@@ -4,9 +4,11 @@ from nose.tools import raises
 from flufl.enum import IntEnum
 import quantities as pq
 
+
 def test_convert_boolean():
     assert ik.toptica.convert_toptica_boolean("bloof") == False
     assert ik.toptica.convert_toptica_boolean("boot") == True
+
 
 @raises(ValueError)
 def test_convert_boolean_value():
@@ -17,7 +19,7 @@ def test_serial_number():
     with expected_protocol(
         ik.toptica.TopMode,
         ["(param-ref 'laser1:serial-number)", "(param-ref 'laser2:serial-number)"],
-        ["bloop1>", "bloop2>"],
+        ["(param-ref 'laser1:serial-number)\nbloop1>", "(param-ref 'laser2:serial-number)\nbloop2>"],
         sep="\n"
     ) as tm:
         assert tm.laser1.serial_number == "bloop1"
@@ -28,7 +30,7 @@ def test_model():
     with expected_protocol(
         ik.toptica.TopMode,
         ["(param-ref 'laser1:model)", "(param-ref 'laser2:model)"],
-        ["bloop1>", "bloop2>"],
+        ["(param-ref 'laser1:model)\nbloop1>", "(param-ref 'laser2:model)\nbloop2>"],
         sep="\n"
     ) as tm:
         assert tm.laser1.model == "bloop1"
@@ -39,7 +41,7 @@ def test_wavelength():
     with expected_protocol(
         ik.toptica.TopMode,
         ["(param-ref 'laser1:wavelength)", "(param-ref 'laser2:wavelength)"],
-        ["640>", "405.3>"],
+        ["(param-ref 'laser1:wavelength)\n640>", "(param-ref 'laser2:wavelength)\n405.3>"],
         sep="\n"
     ) as tm:
         assert tm.laser1.wavelength == 640*pq.nm
@@ -50,7 +52,7 @@ def test_laser_enable():
     with expected_protocol(
         ik.toptica.TopMode,
         ["(param-ref 'laser1:emission)", "(param-set! 'laser1:enable-emission #t)"],
-        ["#f>", ">"],
+        ["(param-ref 'laser1:emission)\n#f>", "(param-set! 'laser1:enable-emission #t)\n>"],
         sep="\n"
     ) as tm:
         assert tm.laser1.enable == False
@@ -61,7 +63,7 @@ def test_laser_ontime():
     with expected_protocol(
         ik.toptica.TopMode,
         ["(param-ref 'laser1:ontime)"],
-        ["10000>"],
+        ["(param-ref 'laser1:ontime)\n10000>"],
         sep="\n"
     ) as tm:
         assert tm.laser1.on_time == 10000*pq.s
@@ -71,7 +73,7 @@ def test_laser_charm_status():
     with expected_protocol(
         ik.toptica.TopMode,
         ["(param-ref 'laser1:health)"],
-        ["230>"],
+        ["(param-ref 'laser1:health)\n230>"],
         sep="\n"
     ) as tm:
         assert tm.laser1.charm_status == 1
@@ -81,7 +83,7 @@ def test_laser_temperature_control_status():
     with expected_protocol(
         ik.toptica.TopMode,
         ["(param-ref 'laser1:health)"],
-        ["230>"],
+        ["(param-ref 'laser1:health)\n230>"],
         sep="\n"
     ) as tm:
         assert tm.laser1.temperature_control_status == 1
@@ -91,7 +93,7 @@ def test_laser_current_control_status():
     with expected_protocol(
         ik.toptica.TopMode,
         ["(param-ref 'laser1:health)"],
-        ["230>"],
+        ["(param-ref 'laser1:health)\n230>"],
         sep="\n"
     ) as tm:
         assert tm.laser1.current_control_status == 1
