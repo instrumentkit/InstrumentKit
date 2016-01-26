2e4759ea57a5e9fb3f2c07dcd8b19952c160412e
Catherine <milankie@gmail.com>
2016-01-26 11:59:41 -0800

    removed tests (temporarily)

--- instruments/instruments/tests/test_qubitekk/__init__.py
+++ instruments/instruments/tests/test_qubitekk/__init__.py
@@ -32,22 +32,6 @@ import quantities as pq
 
 from nose.tools import raises
 
-## TESTS ######################################################################
 
-test_cc1_name = make_name_test(ik.qubitekk.CC1)
-    
-@raises(IOError)
-def test_cc1_unknown_command():
-    """
-    CC1: Checks that invalid commands are properly turned into exceptions.
-    """
-    with expected_protocol(ik.qubitekk.CC1,
-            [
-                "FTN"
-            ], [
-                "Unknown command"
-            ]
-    ) as cc1:
-        cc1.sendcmd("FTN")
 
         
