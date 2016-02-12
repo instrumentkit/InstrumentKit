61875d6f8a82e2ab9f319bf6233696ce6e767a4a
Steven Casagrande <scasagrande@galvant.ca>
2016-02-11 22:57:32 -0500

    Lint oxford itc 503

--- instruments/instruments/oxford/__init__.py
+++ instruments/instruments/oxford/__init__.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""
+Module containing Oxford instruments
+"""
+
 
 from __future__ import absolute_import
 
-from instruments.oxford.oxforditc503 import OxfordITC503
+from .oxforditc503 import OxfordITC503
