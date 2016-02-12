b4df0bf5da21b667dcc5b78927f98b07bb1e6f28
Steven Casagrande <scasagrande@galvant.ca>
2016-02-12 09:11:35 -0500

    autopep8 phase matrix files

--- instruments/instruments/phasematrix/phasematrix_fsw0020.py
+++ instruments/instruments/phasematrix/phasematrix_fsw0020.py
@@ -1,29 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-##
-# phasematrix_fsw0020.py: "Native SPI" driver for Phase Matrix signal
-#     generators.
-##
-# © 2013 Steven Casagrande (scasagrande@galvant.ca).
-#
-# This file is a part of the InstrumentKit project.
-# Licensed under the AGPL version 3.
-##
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-##
+"""
+Provides support for the Phase Matrix FSW0020 signal generator.
+"""
 
-## IMPORTS #####################################################################
+# IMPORTS #####################################################################
 
 from __future__ import absolute_import
 from __future__ import division
@@ -35,14 +16,15 @@ from quantities.unitquantity import IrreducibleUnit
 from instruments.units import *
 
 
-## CLASSES #####################################################################
+# CLASSES #####################################################################
 
 class PhaseMatrixFSW0020(SingleChannelSG):
+
     """
     Communicates with a Phase Matrix FSW-0020 signal generator via the
     "Native SPI" protocol, supported on all FSW firmware versions.
     """
-        
+
     def reset(self):
         r"""
         Causes the connected signal generator to perform a hardware reset.
@@ -50,19 +32,19 @@ class PhaseMatrixFSW0020(SingleChannelSG):
         :math:`5 \mu\text{s}`.
         """
         self.sendcmd('0E.')
-    
+
     @property
     def freq(self):
         """
         Gets/sets the output frequency of the signal generator.
         If units are not specified, the frequency is assumed
         to be in gigahertz (GHz).
-        
+
         :type: `~quantities.Quantity` or `float`
         :units: frequency, assumed to be GHz
         """
         return (int(self.query('04.'), 16) * mHz).rescale(GHz)
-        
+
     @freq.setter
     def freq(self, newval):
         # Rescale the input to millihertz as demanded by the signal
@@ -72,7 +54,7 @@ class PhaseMatrixFSW0020(SingleChannelSG):
         # Write the integer to the serial port in ASCII-encoded
         # uppercase-hexadecimal format, with padding to 12 nybbles.
         self.sendcmd('0C{:012X}.'.format(newval))
-        
+
         # No return data, so no readline needed.
 
     @property
@@ -81,7 +63,7 @@ class PhaseMatrixFSW0020(SingleChannelSG):
         Gets/sets the output power of the signal generator.
         If units are not specified, the power is assumed to be in
         decibel-milliwatts (dBm).
-        
+
         :type: `~quantities.Quantity` or `float`
         :units: log-power, assumed to be dBm
         """
@@ -98,10 +80,11 @@ class PhaseMatrixFSW0020(SingleChannelSG):
 
         # Command code 0x03, parameter length 2 bytes (4 nybbles)
         self.sendcmd('03{:04X}.'.format(newval))
-        
+
     @property
     def phase(self):
         raise NotImplementedError
+
     @phase.setter
     def phase(self, newval):
         raise NotImplementedError
@@ -130,7 +113,6 @@ class PhaseMatrixFSW0020(SingleChannelSG):
     def ref_output(self, newval):
         self.sendcmd('08{:02X}.'.format(1 if newval else 0))
 
-
     @property
     def output(self):
         """
@@ -142,7 +124,6 @@ class PhaseMatrixFSW0020(SingleChannelSG):
     @output.setter
     def output(self, newval):
         self.sendcmd('0F{:02X}.'.format(1 if newval else 0))
-
 
     @property
     def pulse_modulation(self):
@@ -167,4 +148,3 @@ class PhaseMatrixFSW0020(SingleChannelSG):
     @am_modulation.setter
     def am_modulation(self, newval):
         self.sendcmd('0A{:02X}.'.format(1 if newval else 0))
-    
