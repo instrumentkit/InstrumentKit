3c8d75a18081610e851177101fc08b9de065d7c8
Steven Casagrande <scasagrande@galvant.ca>
2016-02-16 09:11:17 -0500

    autopep8 pass on srs files

--- instruments/instruments/srs/srsctc100.py
+++ instruments/instruments/srs/srsctc100.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-##
+#
 # srsctc100.py: Class for communicating with the SRS CTC-100.
-##
+#
 # © 2013 Steven Casagrande (scasagrande@galvant.ca).
 #
 # This file is a part of the InstrumentKit project.
 # Licensed under the AGPL version 3.
-##
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -20,9 +20,9 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
-##
+#
 
-## IMPORTS #####################################################################
+# IMPORTS #####################################################################
 
 from __future__ import absolute_import
 from __future__ import division
@@ -38,25 +38,26 @@ from instruments.generic_scpi import SCPIInstrument
 from instruments.util_fns import assume_units, ProxyList
 import sys
 
-## CLASSES #####################################################################
+# CLASSES #####################################################################
+
 
 class SRSCTC100(SCPIInstrument):
+
     """
     Communicates with a Stanford Research Systems CTC-100 cryogenic temperature
     controller.
     """
-                 
+
     def __init__(self, filelike):
         super(SRSCTC100, self).__init__(filelike)
         self._do_errcheck = True
-    
-    
-    ## DICTIONARIES ##
+
+    # DICTIONARIES ##
     _BOOL_NAMES = {
         'On': True,
         'Off': False
     }
-    
+
     # Note that the SRS CTC-100 uses '\xb0' to represent '°'.
     _UNIT_NAMES = {
         '\xb0C': pq.celsius,
@@ -65,58 +66,60 @@ class SRSCTC100(SCPIInstrument):
         '\xea':  pq.ohm,
         '':      pq.dimensionless
     }
-    
-    
-    ## INNER CLASSES ##
-    
+
+    # INNER CLASSES ##
+
     class SensorType(Enum):
         rtd = 'RTD'
         thermistor = 'Thermistor'
         diode = 'Diode'
         rox = 'ROX'
-    
+
     class Channel(object):
+
         """
         Represents an input or output channel on an SRS CTC-100 cryogenic
         temperature controller.
         """
-        
+
         def __init__(self, ctc, chan_name):
             self._ctc = ctc
-            
+
             # Save the pretty name that we are given.
             self._chan_name = chan_name
-            
+
             # Strip spaces from the name used in remote programming, as
             # specified on page 14 of the manual.
             self._rem_name = chan_name.replace(" ", "")
-        
-        ## PRIVATE METHODS ##
+
+        # PRIVATE METHODS ##
         def _get(self, prop_name):
             return self._ctc.query("{}.{}?".format(
                 self._rem_name,
                 prop_name
             )).strip()
-            
+
         def _set(self, prop_name, newval):
-            self._ctc.sendcmd('{}.{} = "{}"'.format(self._rem_name, prop_name, newval))
-        
-        ## DISPLAY AND PROGRAMMING ##
+            self._ctc.sendcmd(
+                '{}.{} = "{}"'.format(self._rem_name, prop_name, newval))
+
+        # DISPLAY AND PROGRAMMING ##
         # These properties control how the channel is identified in scripts
         # and on the front-panel display.
-            
+
         @property
         def name(self):
             return self._chan_name
+
         @name.setter
         def name(self, newval):
             self._set('name', newval)
             # TODO: check for errors!
             self._chan_name = newval
             self._rem_name = newval.replace(" ", "")
-            
-        ## BASICS ##
-            
+
+        # BASICS ##
+
         @property
         def value(self):
             # WARNING: Queries all units all the time.
@@ -126,58 +129,61 @@ class SRSCTC100(SCPIInstrument):
                 float(self._get('value')),
                 self.units
             )
-            
+
         @property
         def units(self):
             # FIXME: does not respect "chan.d/dt" property.
             return self._ctc._channel_units()[self._chan_name]
             # FIXME: the following line doesn't do what I'd expect, and so it's
             #        commented out.
-            # return self._ctc._UNIT_NAMES[self._ctc.query('{}.units?'.format(self._rem_name)).strip()]
-            
+            # return
+            # self._ctc._UNIT_NAMES[self._ctc.query('{}.units?'.format(self._rem_name)).strip()]
+
         @property
         def sensor_type(self):
             return self._ctc.SensorType(self._get('sensor'))
-            
-        ## STATS ##
+
+        # STATS ##
         # The following properties control and query the statistics of the
         # channel.
-   
+
         @property
         def stats_enabled(self):
             return self._ctc._BOOL_NAMES[self._get('stats')]
+
         @stats_enabled.setter
         def stats_enabled(self, newval):
             # FIXME: replace with inverse dict.
             self._set('stats', 'On' if newval else 'Off')
-            
+
         @property
         def stats_points(self):
             return int(self._get('points'))
+
         @stats_points.setter
         def stats_points(self, newval):
             self._set('points', int(newval))
-            
+
         @property
         def average(self):
             return pq.Quantity(
                 float(self._get('average')),
                 self.units
             )
-            
+
         @property
         def std_dev(self):
             return pq.Quantity(
                 float(self._get('SD')),
                 self.units
             )
-            
-        ## LOGGING ##
-    
+
+        # LOGGING ##
+
         def get_log_point(self, which='next', units=None):
             if units is None:
                 units = self.units
-                
+
             point = [
                 s.strip() for s in
                 self._ctc.query(
@@ -185,43 +191,42 @@ class SRSCTC100(SCPIInstrument):
                 ).split(',')
             ]
             return pq.Quantity(point[0], 'ms'), pq.Quantity(point[1], units)
-            
+
         def get_log(self):
             # Remember the current units.
             units = self.units
-        
+
             # Find out how many points there are.
-            n_points = int(self._ctc.query('getLog.xy? {}'.format(self._chan_name)))
-            
+            n_points = int(
+                self._ctc.query('getLog.xy? {}'.format(self._chan_name)))
+
             # Make an empty quantity that size for the times and for the channel
             # values.
             ts = pq.Quantity(np.empty((n_points,)), 'ms')
             temps = pq.Quantity(np.empty((n_points,)), units)
-            
+
             # Reset the position to the first point, then save it.
             with self._ctc._error_checking_disabled():
                 ts[0], temps[0] = get_log_point('first', units)
                 for idx in range(1, n_points):
                     ts[idx], temps[idx] = get_log_point('next', units)
-            
+
             # Do an actual error check now.
             if self._ctc._do_errcheck:
                 self._ctc._errcheck()
-                
+
             return ts, temps
-                
-            
-   
-    ## PRIVATE METHODS ##
-        
+
+    # PRIVATE METHODS ##
+
     def _channel_names(self):
         """
         Returns the names of valid channels, using the ``getOutput.names``
         command, as documented in the example on page 14 of the
         `CTC-100 manual`_.
-        
+
         Note that ``getOutput`` also lists input channels, confusingly enough.
-        
+
         .. _CTC-100 manual: http://www.thinksrs.com/downloads/PDFs/Manuals/CTC100m.pdf
         """
         # We need to split apart the comma-separated list and make sure that
@@ -238,7 +243,7 @@ class SRSCTC100(SCPIInstrument):
             for name in self.query('getOutput.names?').split(',')
         ]
         return names
-    
+
     def _channel_units(self):
         """
         Returns a dictionary from channel names to channel units, using the
@@ -254,7 +259,7 @@ class SRSCTC100(SCPIInstrument):
             (chan_name, self._UNIT_NAMES[unit_str])
             for chan_name, unit_str in zip(self._channel_names(), unit_strings)
         )
-        
+
     def _errcheck(self):
         errs = super(SRSCTC100, self).query('geterror?').strip()
         err_code, err_descript = errs.split(',')
@@ -263,46 +268,48 @@ class SRSCTC100(SCPIInstrument):
             return
         else:
             raise IOError(err_descript.strip())
-            
+
     @contextmanager
     def _error_checking_disabled(self):
         old = self._do_errcheck
         self._do_errcheck = False
         yield
         self._do_errcheck = old
-   
-    ## PROPERTIES ##
+
+    # PROPERTIES ##
     @property
     def channel(self):
         # Note that since the names can change, we need to query channel names
         # each time. This is inefficient, but alas.
         return ProxyList(self, self.Channel, self._channel_names())
-        
+
     @property
     def display_figures(self):
         return int(self.query('system.display.figures?'))
+
     @display_figures.setter
     def display_figures(self, newval):
         if newval not in range(7):
-            raise ValueError("Number of display figures must be an integer from 0 to 6, inclusive.")
+            raise ValueError(
+                "Number of display figures must be an integer from 0 to 6, inclusive.")
         self.sendcmd('system.display.figures = {}'.format(newval))
-        
-    ## OVERRIDEN METHODS ##
-    
-    # We override sendcmd() and query() to do error checking after each command.
+
+    # OVERRIDEN METHODS ##
+
+    # We override sendcmd() and query() to do error checking after each
+    # command.
     def sendcmd(self, cmd):
         super(SRSCTC100, self).sendcmd(cmd)
         if self._do_errcheck:
             self._errcheck()
-        
+
     def query(self, cmd):
         resp = super(SRSCTC100, self).query(cmd)
         if self._do_errcheck:
             self._errcheck()
         return resp
-    
-    ## LOGGING COMMANDS ##
-    
+
+    # LOGGING COMMANDS ##
+
     def clear_log(self):
         self._ctc.sendcmd('System.Log.Clear yes')
-    
