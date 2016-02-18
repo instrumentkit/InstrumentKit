56ac1320f4f775db09b3a3b51d0a8d50a04346af
Steven Casagrande <scasagrande@galvant.ca>
2016-02-17 23:38:22 -0500

    Remove channel object from init import

--- instruments/instruments/srs/__init__.py
+++ instruments/instruments/srs/__init__.py
@@ -8,8 +8,5 @@ from __future__ import absolute_import
 
 from .srs345 import SRS345
 from .srs830 import SRS830
-from .srsdg645 import (
-    SRSDG645,
-    _SRSDG645Channel,
-)
+from .srsdg645 import SRSDG645
 from .srsctc100 import SRSCTC100
