99b1af224311b8e21e3fbe51eeaa2a8a3d3b0f58
Steven Casagrande <scasagrande@galvant.ca>
2016-03-05 10:52:49 -0500

    lint socket communicator

--- instruments/instruments/abstract_instruments/comm/socket_communicator.py
+++ instruments/instruments/abstract_instruments/comm/socket_communicator.py
@@ -1,27 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# socket_communicator.py: Encapsulates a socket into a filelike object.
-#
-# © 2013-2015 Steven Casagrande (scasagrande@galvant.ca).
-#        Chris Granade (cgranade@cgranade.com).
-#
-# This file is a part of the InstrumentKit project.
-# Licensed under the AGPL version 3.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
+"""
+Provides a tcpip socket communicator for connecting with instruments over
+raw ethernet connections.
+"""
 
 # IMPORTS #####################################################################
 
@@ -31,7 +13,6 @@ from __future__ import division
 import io
 import socket
 
-import numpy as np
 import quantities as pq
 
 from instruments.abstract_instruments.comm import AbstractCommunicator
@@ -53,23 +34,23 @@ class SocketCommunicator(io.IOBase, AbstractCommunicator):
         AbstractCommunicator.__init__(self)
         if isinstance(conn, socket.socket):
             self._conn = conn
-            self._terminator = '\n'
+            self._terminator = "\n"
         else:
-            raise TypeError(
-                'SocketCommunicator must wrap a socket.socket object.')
+            raise TypeError("SocketCommunicator must wrap a "
+                            ":class:`socket.socket` object.")
 
-    # PROPERTIES ##
+    # PROPERTIES #
 
     @property
     def address(self):
-        '''
+        """
         Returns the socket peer address information as a tuple.
-        '''
+        """
         return self._conn.getpeername()
 
     @address.setter
     def address(self, newval):
-        raise NotImplementedError('Unable to change address of sockets.')
+        raise NotImplementedError("Unable to change address of sockets.")
 
     @property
     def terminator(self):
@@ -78,15 +59,21 @@ class SocketCommunicator(io.IOBase, AbstractCommunicator):
     @terminator.setter
     def terminator(self, newval):
         if not isinstance(newval, str):
-            raise TypeError('Terminator for SocketCommunicator must be '
-                            'specified as a single character string.')
+            raise TypeError("Terminator for SocketCommunicator must be "
+                            "specified as a single character string.")
         if len(newval) > 1:
-            raise ValueError('Terminator for SocketCommunicator must only be 1 '
-                             'character long.')
+            raise ValueError("Terminator for SocketCommunicator must only be 1 "
+                             "character long.")
         self._terminator = newval
 
     @property
     def timeout(self):
+        """
+        Gets/sets the connection timeout of the socket comm channel.
+
+        :type: `~quantities.Quantity`
+        :units: As specified or assumed to be of units ``seconds``
+        """
         return self._conn.gettimeout() * pq.second
 
     @timeout.setter
@@ -94,18 +81,29 @@ class SocketCommunicator(io.IOBase, AbstractCommunicator):
         newval = assume_units(newval, pq.second).rescale(pq.second).magnitude
         self._conn.settimeout(newval)
 
-    # FILE-LIKE METHODS ##
+    # FILE-LIKE METHODS #
 
     def close(self):
+        """
+        Shutdown and close the `socket.socket` connection.
+        """
         try:
             self._conn.shutdown()
         finally:
             self._conn.close()
 
     def read(self, size):
-        if (size >= 0):
+        """
+        Read bytes in from the socket connection.
+
+        :param int size: The number of bytes to read in from the socket
+            connection.
+        :return: The read bytes
+        :rtype: `str`
+        """
+        if size >= 0:
             return self._conn.recv(size)
-        elif (size == -1):
+        elif size == -1:
             result = bytearray()
             c = 0
             while c != self._terminator:
@@ -113,35 +111,67 @@ class SocketCommunicator(io.IOBase, AbstractCommunicator):
                 result += c
             return bytes(result)
         else:
-            raise ValueError('Must read a positive value of characters.')
+            raise ValueError("Must read a positive value of characters.")
 
-    def write(self, string):
-        self._conn.sendall(string)
+    def write(self, msg):
+        """
+        Write bytes to the `socket.socket` connection object.
 
-    def seek(self, offset):
+        :param str msg: Bytes to be sent to the instrument over the socket
+            connection.
+        """
+        self._conn.sendall(msg)
+
+    def seek(self, offset):  # pylint: disable=unused-argument,no-self-use
+        """
+        Go to a specific offset for the input data source.
+
+        Not implemented for socket communicator.
+        """
         return NotImplemented
 
-    def tell(self):
+    def tell(self):  # pylint: disable=no-self-use
+        """
+        Get the current positional offset for the input data source.
+
+        Not implemented for socket communicator.
+        """
         return NotImplemented
 
     def flush_input(self):
-        '''
+        """
         Instruct the communicator to flush the input buffer, discarding the
         entirety of its contents.
-        '''
+        """
         _ = self.read(-1)  # Read in everything in the buffer and trash it
 
-    # METHODS ##
+    # METHODS #
 
     def _sendcmd(self, msg):
-        '''
-        '''
-        msg = msg + self._terminator
+        """
+        This is the implementation of ``sendcmd`` for communicating with
+        socket connections. This function is in turn wrapped by the concrete
+        method `AbstractCommunicator.sendcmd` to provide consistent logging
+        functionality across all communication layers.
+
+        :param str msg: The command message to send to the instrument
+        """
+        msg += self._terminator
         self._conn.sendall(msg)
 
     def _query(self, msg, size=-1):
-        '''
-        '''
+        """
+        This is the implementation of ``query`` for communicating with
+        socket connections. This function is in turn wrapped by the concrete
+        method `AbstractCommunicator.query` to provide consistent logging
+        functionality across all communication layers.
+
+        :param str msg: The query message to send to the instrument
+        :param int size: The number of bytes to read back from the instrument
+            response.
+        :return: The instrument response to the query
+        :rtype: `str`
+        """
         self.sendcmd(msg)
         resp = self.read(size)
         return resp
