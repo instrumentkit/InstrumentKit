485538e5bef0b875aa9544fbb8902f1e776e5ad8
Steven Casagrande <scasagrande@galvant.ca>
2016-03-25 22:34:30 -0400

    More merge conflict changes

--- instruments/abstract_instruments/comm/vxi11_communicator.py
+++ instruments/abstract_instruments/comm/vxi11_communicator.py
@@ -1,27 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# vxi11_communicator.py: Communicator that uses python-vxi11 to interface
-#     with VXI11 devices.
-#
-# © 2016 Steven Casagrande (scasagrande@galvant.ca).
-#
-# This file is a part of the InstrumentKit project.
-# Licensed under the AGPL version 3.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-#
+"""
+Provides a communication layer that uses python-vxi11 to interface with
+VXI11 devices.
+"""
 
 # IMPORTS #####################################################################
 
--- instruments/tests/test_comm/test_vxi11.py
+++ instruments/tests/test_comm/test_vxi11.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # test_vxi11.py: Tests VXI11Communicator
