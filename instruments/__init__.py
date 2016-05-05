7220a93ee68bcba14c980b2cc63ad704ef4eb41f
Steven Casagrande <scasagrande@galvant.ca>
2016-05-04 20:52:02 -0400

    Bump version 0.0.3 -> 0.0.4

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -36,7 +36,7 @@ from .config import load_instruments
 # In keeping with PEP-396, we define a version number of the form
 # {major}.{minor}[.{postrelease}]{prerelease-tag}
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 __title__ = "instrumentkit"
 __description__ = "Test and measurement communication library"
