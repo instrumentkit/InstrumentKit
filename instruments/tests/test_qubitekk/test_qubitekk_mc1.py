b62c62578725899e6951d151a5a0b9cc0d2aa051
Catherine Holloway <milankie@gmail.com>
2016-06-02 14:32:07 -0700

    cleaning up whitespace and imports

--- instruments/tests/test_qubitekk/test_qubitekk_mc1.py
+++ instruments/tests/test_qubitekk/test_qubitekk_mc1.py
@@ -9,25 +9,25 @@ Module containing tests for the Qubitekk MC1
 from __future__ import absolute_import
 
 from nose.tools import raises
-import quantities as pq
 
 import instruments as ik
-from instruments.tests import expected_protocol, unit_eq
+from instruments.tests import expected_protocol
+
 
 # TESTS ######################################################################
 
 
 def test_mc1_setting():
     with expected_protocol(
-        ik.qubitekk.MC1,
-        [
-            "OUTP?",
-            ":OUTP 0"
-        ],
-        [
-            "1"
-        ],
-        sep="\r"
+            ik.qubitekk.MC1,
+            [
+                "OUTP?",
+                ":OUTP 0"
+            ],
+            [
+                "1"
+            ],
+            sep="\r"
     ) as mc:
         assert mc.setting == 1
         mc.setting = 0
@@ -35,80 +35,80 @@ def test_mc1_setting():
 
 def test_mc1_position():
     with expected_protocol(
-        ik.qubitekk.MC1,
-        [
-            "POSI?"
-        ],
-        [
-            "-100"
-        ],
-        sep="\r"
+            ik.qubitekk.MC1,
+            [
+                "POSI?"
+            ],
+            [
+                "-100"
+            ],
+            sep="\r"
     ) as mc:
         assert mc.position == -100
 
 
 def test_mc1_direction():
     with expected_protocol(
-        ik.qubitekk.MC1,
-        [
-            "DIRE?"
-        ],
-        [
-            "-100"
-        ],
-        sep="\r"
+            ik.qubitekk.MC1,
+            [
+                "DIRE?"
+            ],
+            [
+                "-100"
+            ],
+            sep="\r"
     ) as mc:
         assert mc.direction == -100
 
 
 def test_mc1_firmware():
     with expected_protocol(
-        ik.qubitekk.MC1,
-        [
-            "FIRM?"
-        ],
-        [
-            "1.0.1"
-        ],
-        sep="\r"
+            ik.qubitekk.MC1,
+            [
+                "FIRM?"
+            ],
+            [
+                "1.0.1"
+            ],
+            sep="\r"
     ) as mc:
         assert mc.firmware == (1, 0, 1)
 
 
 def test_mc1_motor():
     with expected_protocol(
-        ik.qubitekk.MC1,
-        [
-            "MOTO?"
-        ],
-        [
-            "Radio"
-        ],
-        sep="\r"
+            ik.qubitekk.MC1,
+            [
+                "MOTO?"
+            ],
+            [
+                "Radio"
+            ],
+            sep="\r"
     ) as mc:
         assert mc.controller == mc.MotorType.radio
 
 
 def test_mc1_move_timeout():
     with expected_protocol(
-        ik.qubitekk.MC1,
-        [
-            "TAME?"
-        ],
-        [
-            "200"
-        ],
-        sep="\r"
+            ik.qubitekk.MC1,
+            [
+                "TAME?"
+            ],
+            [
+                "200"
+            ],
+            sep="\r"
     ) as mc:
         assert mc.move_timeout == 200
 
 
 def test_mc1_range():
     with expected_protocol(
-        ik.qubitekk.MC1,
-        [],
-        [],
-        sep="\r"
+            ik.qubitekk.MC1,
+            [],
+            [],
+            sep="\r"
     ) as mc:
         mc.upper_limit = 200
         mc.lower_limit = 0
@@ -118,50 +118,50 @@ def test_mc1_range():
 
 def test_mc1_is_centering():
     with expected_protocol(
-        ik.qubitekk.MC1,
-        ["CENT?"],
-        ["1"],
-        sep="\r"
+            ik.qubitekk.MC1,
+            ["CENT?"],
+            ["1"],
+            sep="\r"
     ) as mc:
         assert mc.is_centering()
 
 
 def test_mc1_center():
-     with expected_protocol(
-        ik.qubitekk.MC1,
-        [":CENT"],
-        [""],
-        sep="\r"
+    with expected_protocol(
+            ik.qubitekk.MC1,
+            [":CENT"],
+            [""],
+            sep="\r"
     ) as mc:
         mc.center()
 
 
 def test_mc1_reset():
-     with expected_protocol(
-        ik.qubitekk.MC1,
-        [":RESE"],
-        [""],
-        sep="\r"
+    with expected_protocol(
+            ik.qubitekk.MC1,
+            [":RESE"],
+            [""],
+            sep="\r"
     ) as mc:
         mc.reset()
 
 
 def test_mc1_move():
-     with expected_protocol(
-        ik.qubitekk.MC1,
-        [":MOVE 0"],
-        [""],
-        sep="\r"
+    with expected_protocol(
+            ik.qubitekk.MC1,
+            [":MOVE 0"],
+            [""],
+            sep="\r"
     ) as mc:
         mc.move(0)
 
 
 @raises(ValueError)
 def test_mc1_move_value_error():
-     with expected_protocol(
-        ik.qubitekk.MC1,
-        [":MOVE -1000"],
-        [""],
-        sep="\r"
+    with expected_protocol(
+            ik.qubitekk.MC1,
+            [":MOVE -1000"],
+            [""],
+            sep="\r"
     ) as mc:
         mc.move(-1000)
