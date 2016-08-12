79f72a3b55b5c62429e50ed8b7d072f4ae09851b
Catherine Holloway <milankie@gmail.com>
2016-08-12 11:50:24 -0700

    cleaning up examples

--- doc/examples/qubitekk/ex_qubitekk_mc1.py
+++ doc/examples/qubitekk/ex_qubitekk_mc1.py
@@ -1,49 +1,14 @@
-from sys import platform
+#!/usr/bin/python
+# Qubitekk Motor controller example
 from time import sleep
-from serial.tools import list_ports
 
 from instruments import Device
 from instruments.qubitekk import MC1
 
-HARDWARE_ID = [1027, 24577]
 
-
-def find_port():
-    com_port = None
-    found_port = False
-    for port in list_ports.comports():
-        try:
-            vid = port.vid
-            pid = port.pid
-            serial_number = port.serial_number
-            device = port.device
-        except Exception as e:
-            if port[2] == 'n/a':
-                continue
-            serial_number = port[2].split("=")[2]
-            vid = int(port[2].split("=")[1].split(":")[0], 16)
-            pid = int(port[2].split("=")[1].split(":")[1].split(" ")[0], 16)
-            device = port[0]
-        print(vid, pid, serial_number, device)
-        if len(HARDWARE_ID) == 2:
-            if vid == HARDWARE_ID[0] and pid == HARDWARE_ID[1]:
-                found_port = True
-                com_port = device
-                break
-        else:
-            if serial_number == HARDWARE_ID[2]:
-                found_port = True
-                com_port = device
-                break
-    if not found_port:
-        raise RuntimeError("device matching ids for motor controller not found")
-    return com_port
-
-
-def main():
+if __name__ == "__main__":
     hardware = Device(vid=1027, pid=24577)
     mc1 = MC1.open_serial(hardware.port, baud=9600, timeout=1)
-
     print("Firmware", mc1.firmware)
     print("Motor controller type: ", mc1.controller)
     print("centering")
@@ -59,13 +24,9 @@ def main():
     mc1.lower_limit = -260
     mc1.upper_limit = 300
     mc1.increment = 5
-    for x_pos in mc1.range:
+    for x_pos in range(mc1.lower_limit, mc1.upper_limit, step=mc1.increment):
         print(str(mc1.position)+" "+str(mc1.direction))
         mc1.move(x_pos)
         while mc1.move_timeout > 0:
             sleep(0.5)
         sleep(1)
-
-
-if __name__ == "__main__":
-    main()
