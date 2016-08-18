e5ab296636e2efc3f1458cccec13d871eeaddfab
Catherine Holloway <milankie@gmail.com>
2016-08-18 08:52:56 -0700

    changed motor example

--- doc/examples/qubitekk/ex_qubitekk_mc1.py
+++ doc/examples/qubitekk/ex_qubitekk_mc1.py
@@ -7,8 +7,16 @@ from instruments.qubitekk import MC1
 
 
 if __name__ == "__main__":
-    hardware = Device(vid=1027, pid=24577)
+    # ftdi chip
+    #hardware = Device(vid=1027, pid=24577)
+    # teensy
+    hardware = Device(vid=5824, pid=1155)
     mc1 = MC1.open_serial(hardware.port, baud=9600, timeout=1)
+    mc1.step_size = 25
+    mc1.inertia = 10
+    print("step size:", mc1.step_size)
+    print("inertial force: ", mc1.inertia)
+    '''
     print("Firmware", mc1.firmware)
     print("Motor controller type: ", mc1.controller)
     print("centering")
@@ -24,9 +32,10 @@ if __name__ == "__main__":
     mc1.lower_limit = -260
     mc1.upper_limit = 300
     mc1.increment = 5
-    for x_pos in range(mc1.lower_limit, mc1.upper_limit, step=mc1.increment):
+    for x_pos in range(mc1.lower_limit, mc1.upper_limit, mc1.increment):
         print(str(mc1.position)+" "+str(mc1.direction))
         mc1.move(x_pos)
         while mc1.move_timeout > 0:
             sleep(0.5)
         sleep(1)
+    '''
