ad1f6960fb05523bdec1aa29843f7587ffb5533b
Steven Casagrande <scasagrande@galvant.ca>
2016-08-18 18:56:55 -0400

    Revert some formatting changes

--- doc/examples/ex_qubitekkcc.py
+++ doc/examples/ex_qubitekkcc.py
@@ -6,8 +6,7 @@ from quantities import second, nanosecond
 
 
 def main():
-    cc = ik.qubitekk.CC1.open_serial(vid=1027, pid=24577, baud=19200,
-                                     timeout=10)
+    cc = ik.qubitekk.CC1.open_serial(vid=1027, pid=24577, baud=19200, timeout=10)
     cc1.dwell_time = 1.0 * second
     print cc1.dwell_time
     cc1.delay = 0.0 * nanosecond
