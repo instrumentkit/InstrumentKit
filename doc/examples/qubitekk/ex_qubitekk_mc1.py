35f7a0f7a237aac939f3383e2d233793762ba0bc
Catherine Holloway <milankie@gmail.com>
2016-08-19 21:45:48 -0700

    adding mc1

---
+++ doc/examples/qubitekk/ex_qubitekk_mc1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# Qubitekk Motor controller example
+from time import sleep
+
+from instruments.qubitekk import MC1
+
+
+if __name__ == "__main__":
+
+    mc1 = MC1.open_serial(vid=1027, pid=24577, baud=9600, timeout=1)
+    mc1.step_size = 25
+    mc1.inertia = 10
+    print("step size:", mc1.step_size)
+    print("inertial force: ", mc1.inertia)
+
+    print("Firmware", mc1.firmware)
+    print("Motor controller type: ", mc1.controller)
+    print("centering")
+
+    mc1.center()
+    while mc1.is_centering():
+        print(str(mc1.metric_position)+" "+str(mc1.direction))
+        pass
+
+    print("Stage Centered")
+    # for the motor in the mechanical delay line, the travel is limited from
+    # the full range of travel. Here's how to set the limits.
+    mc1.lower_limit = -260
+    mc1.upper_limit = 300
+    mc1.increment = 5
+    for x_pos in range(mc1.lower_limit, mc1.upper_limit, mc1.increment):
+        print(str(mc1.metric_position)+" "+str(mc1.direction))
+        mc1.move(x_pos)
+        while mc1.move_timeout > 0:
+            sleep(0.5)
+        sleep(1)
---
+++ instruments/qubitekk/mc1.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Provides support for the Qubitekk MC1 Motor Controller.
+
+MC1 Class originally contributed by Catherine Holloway.
+"""
+
+# IMPORTS #####################################################################
+from __future__ import absolute_import
+from builtins import range
+from enum import Enum
+import quantities as pq
+
+from instruments.abstract_instruments import Instrument
+# CLASSES #####################################################################
+from instruments.util_fns import int_property, enum_property, unitful_property
+
+
+class MC1(Instrument):
+    """
+    The MC1 is a controller for the qubitekk motor controller. Used with a
+    linear actuator to perform a HOM dip.
+    """
+    def __init__(self, filelike, increment=1, upper_limit=300,
+                 lower_limit=-300):
+        super(MC1, self).__init__(filelike)
+        self.terminator = "\r"
+        self.increment = increment
+        self.lower_limit = lower_limit
+        self.upper_limit = upper_limit
+        self._firmware = None
+        self._controller = None
+
+    # ENUMS #
+
+    class MotorType(Enum):
+        """
+        Enum for the motor types for the MC1
+        """
+        radio = "Radio"
+        relay = "Relay"
+
+    # PROPERTIES #
+
+    direction = int_property(
+        name="DIRE",
+        doc="""
+        Get the internal direction variable, which is a function of how far
+        the motor needs to go.
+
+        :type: `int`
+        """,
+        readonly=True
+    )
+
+    inertia = int_property(
+        name="INER",
+        doc="""
+        Gets/Sets the amount of force required to overcome static inertia.
+
+        :type: `int`
+        """,
+        set_fmt=":{} {}"
+    )
+
+    internal_position = int_property(
+        name="POSI",
+        doc="""
+        Get the internal motor state position.
+
+        :type: `int`
+        """,
+        readonly=True
+    )
+
+    metric_position = unitful_property(
+        name="METR",
+        doc="""
+        Get the estimated motor position, in microns.
+
+        :type: `quantities.`
+        """,
+        units=pq.mm,
+        readonly=True
+    )
+
+    setting = int_property(
+        name="OUTP",
+        doc="""
+        Gets/sets the output setting of the optical switch.
+
+        :type: `int`
+        """,
+        valid_set=range(2),
+        set_fmt=":{} {}"
+    )
+
+    step_size = int_property(
+        name="STEP",
+        doc="""
+        Gets/Sets the number of milliseconds per step.
+
+        :type: `int`
+        """,
+        valid_set=range(1, 100),
+        set_fmt=":{} {}"
+    )
+
+    @property
+    def firmware(self):
+        """
+        Gets the firmware version
+
+        :rtype: `tuple`(Major:`int`, Minor:`int`, Patch`int`)
+        """
+        # the firmware is assumed not to change while the device is active
+        # firmware is stored locally as it will be gotten often
+        # pylint: disable=no-member
+        if self._firmware is None:
+            while self._firmware is None:
+                self._firmware = self.query("FIRM?")
+                value = self._firmware.split(".")
+                if len(value) < 3:
+                    for _ in range(3-len(value)):
+                        value.append(0)
+                value = tuple(map(int, value))
+                self._firmware = value
+        return self._firmware
+
+    controller = enum_property(
+        'MOTO',
+        MotorType,
+        doc="""
+        Get the motor controller type.
+        """,
+        readonly=True
+    )
+
+    move_timeout = int_property(
+        name="TIME",
+        doc="""
+        Get the motor's timeout value, which indicates the number of clock
+        cycles before the motor can start moving again.
+
+        :type: `int`
+        """,
+        readonly=True
+    )
+
+    # METHODS #
+
+    def is_centering(self):
+        """
+        Query whether the motor is in its centering phase
+        :return: 0 if not centering, 1 if centering
+        :rtype: int
+        """
+        response = self.query("CENT?")
+        return int(response)
+
+    def center(self):
+        """
+        Commands the motor to go to the center of its travel range
+        """
+        self.sendcmd(":CENT")
+
+    def reset(self):
+        """
+        Sends the stage to the limit of one of its travel ranges
+        """
+        self.sendcmd(":RESE")
+
+    def move(self, new_position):
+        """
+        Move to a specified location. Position is unitless and is defined as
+        the number of motor steps. It varies between motors.
+        :param new_position: the location
+        :type new_position: int
+        """
+        if self.lower_limit <= new_position <= self.upper_limit:
+            cmd = ":MOVE "+str(int(new_position))
+            self.sendcmd(cmd)
+        else:
+            raise ValueError("Location out of range")
