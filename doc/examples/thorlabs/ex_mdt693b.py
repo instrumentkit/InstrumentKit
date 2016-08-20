f9b64f798a96d5bd5ae540b762fe30f7f8ff0df0
Catherine Holloway <milankie@gmail.com>
2016-08-20 13:43:08 -0700

    removed reference to device

--- doc/examples/thorlabs/ex_mdt693b.py
+++ doc/examples/thorlabs/ex_mdt693b.py
@@ -3,10 +3,9 @@ An example script for interacting with the thorlabs MDT693B piezo driver.
 """
 import quantities as pq
 from instruments.thorlabs import MDT693B
-from instruments import Device
 
-hardware = Device(vid=1027, pid=24577)
-mdt = MDT693B.open_serial(hardware.port, baud=115200)
+
+mdt = MDT693B.open_serial(vid=1027, pid=24577, baud=115200)
 
 for i in range(0, 3):
     print("The voltage on ", i, " is ", mdt.channel[i].voltage)
