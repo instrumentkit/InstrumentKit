b1ca84274932b82f57278390a469f875ccdaaaae
Francois Drielsma <francois.drielsma@gmail.com>
2019-08-06 13:02:21 -0700

    Added support for Glassman FR (EJ, ET, EY, FJ) power supplies

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -14,6 +14,7 @@ from .abstract_instruments import Instrument
 from . import agilent
 from . import generic_scpi
 from . import fluke
+from . import glassman
 from . import holzworth
 from . import hp
 from . import keithley
