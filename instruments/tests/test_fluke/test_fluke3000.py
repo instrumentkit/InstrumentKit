d27030241c17fdf88a5b0525fe81d57d670624a2
Francois Drielsma <francois.drielsma@gmail.com>
2019-08-11 11:10:57 -0700

    Indentation style fix in Fluke 3000 tests

--- instruments/tests/test_fluke/test_fluke3000.py
+++ instruments/tests/test_fluke/test_fluke3000.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Module containing tests for the Fluke 3000 FC
+Module containing tests for the Fluke 3000 FC multimeter
 """
 
 # IMPORTS ####################################################################
@@ -18,48 +18,48 @@ from instruments.tests import expected_protocol
 # Empty initialization sequence (scan function) that does not uncover
 # any available Fluke 3000 FC device.
 none_sequence = [
-                    "rfebd 01 0",
-                    "rfebd 02 0",
-                    "rfebd 03 0",
-                    "rfebd 04 0",
-                    "rfebd 05 0",
-                    "rfebd 06 0"
-                ]
+    "rfebd 01 0",
+    "rfebd 02 0",
+    "rfebd 03 0",
+    "rfebd 04 0",
+    "rfebd 05 0",
+    "rfebd 06 0"
+]
 none_response = [
-                    "CR:Ack=2",
-                    "CR:Ack=2",
-                    "CR:Ack=2",
-                    "CR:Ack=2",
-                    "CR:Ack=2",
-                    "CR:Ack=2"
-                ]
+    "CR:Ack=2",
+    "CR:Ack=2",
+    "CR:Ack=2",
+    "CR:Ack=2",
+    "CR:Ack=2",
+    "CR:Ack=2"
+]
 
 # Default initialization sequence (scan function) that binds a multimeter
 # to port 1 and a temperature module to port 2.
 init_sequence = [
-                    "rfebd 01 0",   # 1
-                    "rfgus 01",     # 2
-                    "rfebd 02 0",   # 3
-                    "rfgus 02",     # 4
-                    "rfebd 03 0",   # 5
-                    "rfebd 04 0",   # 6
-                    "rfebd 05 0",   # 7
-                    "rfebd 06 0"    # 8
-                ]
+    "rfebd 01 0",   # 1
+    "rfgus 01",     # 2
+    "rfebd 02 0",   # 3
+    "rfgus 02",     # 4
+    "rfebd 03 0",   # 5
+    "rfebd 04 0",   # 6
+    "rfebd 05 0",   # 7
+    "rfebd 06 0"    # 8
+]
 init_response = [
-                    "CR:Ack=0:RFEBD",   # 1.1
-                    "ME:R:S#=01:DCC=012:PH=64", # 1.2
-                    "CR:Ack=0:RFGUS",   # 2.1
-                    "ME:R:S#=01:DCC=004:PH=46333030304643", # 2.2
-                    "CR:Ack=0:RFEBD",   # 3.1
-                    "ME:R:S#=01:DCC=012:PH=64",  # 3.2
-                    "CR:Ack=0:RFGUS",   # 4.1
-                    "ME:R:S#=02:DCC=004:PH=54333030304643", # 4.2
-                    "CR:Ack=2", # 5
-                    "CR:Ack=2", # 6
-                    "CR:Ack=2", # 7
-                    "CR:Ack=2"  # 8
-                ]
+    "CR:Ack=0:RFEBD",   # 1.1
+    "ME:R:S#=01:DCC=012:PH=64", # 1.2
+    "CR:Ack=0:RFGUS",   # 2.1
+    "ME:R:S#=01:DCC=004:PH=46333030304643", # 2.2
+    "CR:Ack=0:RFEBD",   # 3.1
+    "ME:R:S#=01:DCC=012:PH=64",  # 3.2
+    "CR:Ack=0:RFGUS",   # 4.1
+    "ME:R:S#=02:DCC=004:PH=54333030304643", # 4.2
+    "CR:Ack=2", # 5
+    "CR:Ack=2", # 6
+    "CR:Ack=2", # 7
+    "CR:Ack=2"  # 8
+]
 
 def test_mode():
     with expected_protocol(
