e5845eb470651feead815237f4a139c55a0911ea
Steven Casagrande <scasagrande@galvant.ca>
2020-01-12 21:06:11 -0500

    Update copyrights for 2020 (#227)

--- instruments/__init__.py
+++ instruments/__init__.py
@@ -49,4 +49,4 @@ __author__ = "Steven Casagrande"
 __email__ = "scasagrande@galvant.ca"
 
 __license__ = "AGPLv3"
-__copyright__ = "Copyright (c) 2012-2019 Steven Casagrande"
+__copyright__ = "Copyright (c) 2012-2020 Steven Casagrande"
