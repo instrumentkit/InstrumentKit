7e5e3acc591b1b10380e40f9ebe02984a8cfaf16
Steven Casagrande <scasagrande@galvant.ca>
2020-01-14 23:02:49 -0500

    Fix linting for test_agilent

--- instruments/tests/test_agilent/test_agilent_34410a.py
+++ instruments/tests/test_agilent/test_agilent_34410a.py
@@ -22,56 +22,56 @@ test_agilent_34410a_name = make_name_test(ik.agilent.Agilent34410a)
 
 def test_agilent34410a_read():
     with expected_protocol(
-        ik.agilent.Agilent34410a,
-        [
-            "CONF?",
-            "READ?"
-        ], [
-            "VOLT +1.000000E+01,+3.000000E-06",
-            "+1.86850000E-03"
-        ]
+            ik.agilent.Agilent34410a,
+            [
+                "CONF?",
+                "READ?"
+            ], [
+                "VOLT +1.000000E+01,+3.000000E-06",
+                "+1.86850000E-03"
+            ]
     ) as dmm:
         unit_eq(dmm.read_meter(), +1.86850000E-03 * pq.volt)
 
 
 def test_agilent34410a_data_point_count():
     with expected_protocol(
-        ik.agilent.Agilent34410a,
-        [
-            "DATA:POIN?",
-        ], [
-            "+215",
-        ]
+            ik.agilent.Agilent34410a,
+            [
+                "DATA:POIN?",
+            ], [
+                "+215",
+            ]
     ) as dmm:
         assert dmm.data_point_count == 215
 
 
 def test_agilent34410a_r():
     with expected_protocol(
-        ik.agilent.Agilent34410a,
-        [
-            "CONF?",
-            "FORM:DATA REAL,64",
-            "R? 1"
-        ], [
-            "VOLT +1.000000E+01,+3.000000E-06",
-            # pylint: disable=no-member
-            b"#18" + bytes.fromhex("3FF0000000000000")
-        ]
+            ik.agilent.Agilent34410a,
+            [
+                "CONF?",
+                "FORM:DATA REAL,64",
+                "R? 1"
+            ], [
+                "VOLT +1.000000E+01,+3.000000E-06",
+                # pylint: disable=no-member
+                b"#18" + bytes.fromhex("3FF0000000000000")
+            ]
     ) as dmm:
         unit_eq(dmm.r(1), np.array([1]) * pq.volt)
 
 
 def test_agilent34410a_fetch():
     with expected_protocol(
-        ik.agilent.Agilent34410a,
-        [
-            "CONF?",
-            "FETC?"
-        ], [
-            "VOLT +1.000000E+01,+3.000000E-06",
-            "+4.27150000E-03,5.27150000E-03"
-        ]
+            ik.agilent.Agilent34410a,
+            [
+                "CONF?",
+                "FETC?"
+            ], [
+                "VOLT +1.000000E+01,+3.000000E-06",
+                "+4.27150000E-03,5.27150000E-03"
+            ]
     ) as dmm:
         data = dmm.fetch()
         unit_eq(data[0], 4.27150000E-03 * pq.volt)
@@ -80,15 +80,15 @@ def test_agilent34410a_fetch():
 
 def test_agilent34410a_read_data():
     with expected_protocol(
-        ik.agilent.Agilent34410a,
-        [
-            "CONF?",
-            "FORM:DATA ASC",
-            "DATA:REM? 2"
-        ], [
-            "VOLT +1.000000E+01,+3.000000E-06",
-            "+4.27150000E-03,5.27150000E-03"
-        ]
+            ik.agilent.Agilent34410a,
+            [
+                "CONF?",
+                "FORM:DATA ASC",
+                "DATA:REM? 2"
+            ], [
+                "VOLT +1.000000E+01,+3.000000E-06",
+                "+4.27150000E-03,5.27150000E-03"
+            ]
     ) as dmm:
         data = dmm.read_data(2)
         unit_eq(data[0], 4.27150000E-03 * pq.volt)
@@ -97,14 +97,14 @@ def test_agilent34410a_read_data():
 
 def test_agilent34410a_read_data_nvmem():
     with expected_protocol(
-        ik.agilent.Agilent34410a,
-        [
-            "CONF?",
-            "DATA:DATA? NVMEM",
-        ], [
-            "VOLT +1.000000E+01,+3.000000E-06",
-            "+4.27150000E-03,5.27150000E-03"
-        ]
+            ik.agilent.Agilent34410a,
+            [
+                "CONF?",
+                "DATA:DATA? NVMEM",
+            ], [
+                "VOLT +1.000000E+01,+3.000000E-06",
+                "+4.27150000E-03,5.27150000E-03"
+            ]
     ) as dmm:
         data = dmm.read_data_nvmem()
         unit_eq(data[0], 4.27150000E-03 * pq.volt)
@@ -113,11 +113,11 @@ def test_agilent34410a_read_data_nvmem():
 
 def test_agilent34410a_read_last_data():
     with expected_protocol(
-        ik.agilent.Agilent34410a,
-        [
-            "DATA:LAST?",
-        ], [
-            "+1.73730000E-03 VDC",
-        ]
+            ik.agilent.Agilent34410a,
+            [
+                "DATA:LAST?",
+            ], [
+                "+1.73730000E-03 VDC",
+            ]
     ) as dmm:
         unit_eq(dmm.read_last_data(), 1.73730000E-03 * pq.volt)
