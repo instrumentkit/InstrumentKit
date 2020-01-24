4c57ce81ebf17d0141dfe5ee1dde1a4dec0124ba
Steven Casagrande <scasagrande@galvant.ca>
2020-01-24 00:14:42 -0500

    Add support for Py38

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Operating System :: OS Independent",
     "License :: OSI Approved :: GNU Affero General Public License v3",
     "Intended Audience :: Science/Research",
