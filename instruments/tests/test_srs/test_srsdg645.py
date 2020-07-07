3303cec4433b493af5eac94994d694be2853231b
Reto Trappitsch <10662464+trappitsch@users.noreply.github.com>
2020-07-07 11:20:48 -0700

    Enhancement: SRS645 delay generator added burst mode support (#240)
    * Enhancement: SRS645 delay generator added burst mode support
    
    Added routines for enabling burst mode and controlling all sub
    properties. Set/get implemented for all sub menus of burst mode.
    
    A full test suite for each of the new functions has been implemented
    as well.
    
    Every call has been tested with an actual instrument and is functional.
    
    tox testing: all passed
    
    pylint rating: 10/10
    
    * Fixed singleton-comparison error in pylint
    
    Co-authored-by: Reto Trappitsch <trappitsch@users.noreply.github.com>

--- instruments/tests/test_srs/test_srsdg645.py
+++ instruments/tests/test_srs/test_srsdg645.py
@@ -77,3 +77,97 @@ def test_srsdg645_trigger_source():
         assert ref == ddg.Channels.T0
         assert abs((t - u.Quantity(42, "s")).magnitude) < 1e5
         ddg.channel["B"].delay = (ddg.channel["A"], u.Quantity(1, "minute"))
+
+
+def test_srsdg645_enable_burst_mode():
+    """
+    SRSDG645: Checks getting/setting of enabling burst mode.
+    """
+    with expected_protocol(
+            ik.srs.SRSDG645,
+            [
+                "BURM?",
+                "BURM 1"
+            ],
+            [
+                "0"
+            ]
+    ) as ddg:
+        assert ddg.enable_burst_mode is False
+        ddg.enable_burst_mode = True
+
+
+def test_srsdg645_enable_burst_t0_first():
+    """
+        SRSDG645: Checks getting/setting of enabling T0 output on first
+        in burst mode.
+        """
+    with expected_protocol(
+            ik.srs.SRSDG645,
+            [
+                "BURT?",
+                "BURT 1"
+            ],
+            [
+                "0"
+            ]
+    ) as ddg:
+        assert ddg.enable_burst_t0_first is False
+        ddg.enable_burst_t0_first = True
+
+
+def test_srsdg645_burst_count():
+    """
+        SRSDG645: Checks getting/setting of enabling T0 output on first
+        in burst mode.
+        """
+    with expected_protocol(
+            ik.srs.SRSDG645,
+            [
+                "BURC?",
+                "BURC 42"
+            ],
+            [
+                "10"
+            ]
+    ) as ddg:
+        assert ddg.burst_count == 10
+        ddg.burst_count = 42
+
+
+def test_srsdg645_burst_period():
+    """
+        SRSDG645: Checks getting/setting of enabling T0 output on first
+        in burst mode.
+        """
+    with expected_protocol(
+            ik.srs.SRSDG645,
+            [
+                "BURP?",
+                "BURP 13"
+            ],
+            [
+                "100E-9"
+            ]
+    ) as ddg:
+        unit_eq(ddg.burst_period, u.Quantity(100, "ns").rescale(u.s))
+        ddg.burst_period = u.Quantity(13, "s")
+
+
+def test_srsdg645_burst_delay():
+    """
+        SRSDG645: Checks getting/setting of enabling T0 output on first
+        in burst mode.
+        """
+    with expected_protocol(
+            ik.srs.SRSDG645,
+            [
+                "BURD?",
+                "BURD 42"
+            ],
+            [
+                "0"
+            ]
+    ) as ddg:
+        unit_eq(ddg.burst_delay, u.Quantity(0, "s"))
+        ddg.burst_delay = u.Quantity(42, "s")
