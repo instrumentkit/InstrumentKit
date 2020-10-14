9f83b003873eca8731d1a627f2e18b14d790abcd
Reto Trappitsch <reto@galactic-forensics.space>
2020-10-14 17:43:45 -0400

    Adjust `pytest.approx` statement such that Keithley580 tests pass (#271)
    - Make approx statement with `rel=1e-6` argument, now passes
    - This should be the default according to the docs though...

--- instruments/tests/test_keithley/test_keithley580.py
+++ instruments/tests/test_keithley/test_keithley580.py
@@ -761,7 +761,7 @@ def test_measure(init, create_measurement, resistance):
             sep="\n"
     ) as inst:
         read_value = inst.measure()
-        assert read_value.magnitude == pytest.approx(resistance)
+        assert read_value.magnitude == pytest.approx(resistance, rel=1e-6)
         assert read_value.units == u.ohm
 
 
