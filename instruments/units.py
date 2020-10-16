d4399129c2d58e04fa83c279b3acddc4b470d306
Steven Casagrande <stevencasagrande@gmail.com>
2020-10-16 10:16:30 -0400

    Update units to use pint application registry

--- instruments/units.py
+++ instruments/units.py
@@ -6,12 +6,10 @@ Module containing custom units used by various instruments.
 
 # IMPORTS #####################################################################
 
-# pylint: disable=unused-wildcard-import, wildcard-import
-
-
-from pint import _DEFAULT_REGISTRY as ureg
+import pint
 
 # UNITS #######################################################################
 
+ureg = pint.get_application_registry()
 ureg.define("percent = []")
 ureg.define("centibelmilliwatt = 1e-3 watt; logbase: 10; logfactor: 100 = cBm")
