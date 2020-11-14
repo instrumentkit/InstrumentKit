076e09f16c1fe61a1e6c0d5add96a9044a7dd685
Reto Trappitsch <reto@galactic-forensics.space>
2020-11-14 16:09:28 -0500

    Full coverage for Keithley 2182 & 6220 (#286)
    Added and extended existing tests for these two instruments.

--- instruments/tests/test_keithley/test_keithley6220.py
+++ instruments/tests/test_keithley/test_keithley6220.py
@@ -7,6 +7,8 @@ Unit tests for the Keithley 6220 constant current supply
 # IMPORTS #####################################################################
 
 
+import pytest
+
 from instruments.units import ureg as u
 
 import instruments as ik
@@ -18,6 +20,27 @@ from instruments.tests import expected_protocol
 def test_channel():
     inst = ik.keithley.Keithley6220.open_test()
     assert inst.channel[0] == inst
+
+
+def test_voltage():
+    """Raise NotImplementedError when getting / setting voltage."""
+    with expected_protocol(
+            ik.keithley.Keithley6220,
+            [
+            ],
+            [
+            ]
+    ) as inst:
+        with pytest.raises(NotImplementedError) as err_info:
+            _ = inst.voltage
+        err_msg = err_info.value.args[0]
+        assert err_msg == "The Keithley 6220 does not support voltage " \
+                          "settings."
+        with pytest.raises(NotImplementedError) as err_info:
+            inst.voltage = 42
+        err_msg = err_info.value.args[0]
+        assert err_msg == "The Keithley 6220 does not support voltage " \
+                          "settings."
 
 
 def test_current():
