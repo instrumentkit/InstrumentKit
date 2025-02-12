a8f771d13b537657ce80f42e146b0b7a36f8fd45
Reto Trappitsch <reto@galactic-forensics.space>
2025-02-12 03:30:21 +0100

    Multichannel instrument consistency for optical spectrum analyzer (#431)

--- src/instruments/abstract_instruments/optical_spectrum_analyzer.py
+++ src/instruments/abstract_instruments/optical_spectrum_analyzer.py
@@ -5,10 +5,10 @@ Provides an abstract base class for optical spectrum analyzer instruments
 
 # IMPORTS #####################################################################
 
-
 import abc
 
 from instruments.abstract_instruments import Instrument
+from instruments.util_fns import ProxyList
 
 # CLASSES #####################################################################
 
@@ -21,13 +21,28 @@ class OpticalSpectrumAnalyzer(Instrument, metaclass=abc.ABCMeta):
     provide a consistent interface to the user.
     """
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._channel_count = 1
+
     class Channel(metaclass=abc.ABCMeta):
         """
         Abstract base class for physical channels on an optical spectrum analyzer.
 
         All applicable concrete instruments should inherit from this ABC to
         provide a consistent interface to the user.
+
+        Optical spectrum analyzers that only have a single channel do not need to
+        define their own concrete implementation of this class. Ones with
+        multiple channels need their own definition of this class, where
+        this class contains the concrete implementations of the below
+        abstract methods. Instruments with 1 channel have their concrete
+        implementations at the parent instrument level.
         """
+
+        def __init__(self, parent, name):
+            self._parent = parent
+            self._name = name
 
         # METHODS #
 
@@ -42,7 +57,10 @@ class OpticalSpectrumAnalyzer(Instrument, metaclass=abc.ABCMeta):
             :return: The wavelength component of the waveform.
             :rtype: `numpy.ndarray`
             """
-            raise NotImplementedError
+            if self._parent._channel_count == 1:
+                return self._parent.wavelength(bin_format=bin_format)
+            else:
+                raise NotImplementedError
 
         @abc.abstractmethod
         def data(self, bin_format=True):
@@ -55,22 +73,23 @@ class OpticalSpectrumAnalyzer(Instrument, metaclass=abc.ABCMeta):
             :return: The y-component of the waveform.
             :rtype: `numpy.ndarray`
             """
-            raise NotImplementedError
+            if self._parent._channel_count == 1:
+                return self._parent.data(bin_format=bin_format)
+            else:
+                raise NotImplementedError
 
     # PROPERTIES #
 
     @property
-    @abc.abstractmethod
     def channel(self):
         """
         Gets an iterator or list for easy Pythonic access to the various
         channel objects on the OSA instrument. Typically generated
         by the `~instruments.util_fns.ProxyList` helper.
         """
-        raise NotImplementedError
+        return ProxyList(self, self.Channel, range(self._channel_count))
 
     @property
-    @abc.abstractmethod
     def start_wl(self):
         """
         Gets/sets the the start wavelength of the OSA. This is
@@ -81,12 +100,10 @@ class OpticalSpectrumAnalyzer(Instrument, metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @start_wl.setter
-    @abc.abstractmethod
     def start_wl(self, newval):
         raise NotImplementedError
 
     @property
-    @abc.abstractmethod
     def stop_wl(self):
         """
         Gets/sets the the stop wavelength of the OSA. This is
@@ -97,12 +114,10 @@ class OpticalSpectrumAnalyzer(Instrument, metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @stop_wl.setter
-    @abc.abstractmethod
     def stop_wl(self, newval):
         raise NotImplementedError
 
     @property
-    @abc.abstractmethod
     def bandwidth(self):
         """
         Gets/sets the the bandwidth of the OSA. This is
@@ -113,7 +128,6 @@ class OpticalSpectrumAnalyzer(Instrument, metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @bandwidth.setter
-    @abc.abstractmethod
     def bandwidth(self, newval):
         raise NotImplementedError
 
@@ -125,3 +139,33 @@ class OpticalSpectrumAnalyzer(Instrument, metaclass=abc.ABCMeta):
         Forces a start sweep on the attached OSA.
         """
         raise NotImplementedError
+
+    def wavelength(self, bin_format=True):
+        """
+        Gets the x-axis of the specified data source channel. This is an
+        abstract property.
+
+        :param bool bin_format: If the waveform should be transfered in binary
+            (``True``) or ASCII (``False``) formats.
+        :return: The wavelength component of the waveform.
+        :rtype: `numpy.ndarray`
+        """
+        if self._channel_count > 1:
+            return self.channel[0].wavelength(bin_format=bin_format)
+        else:
+            raise NotImplementedError
+
+    def data(self, bin_format=True):
+        """
+        Gets the y-axis of the specified data source channel. This is an
+        abstract property.
+
+        :param bool bin_format: If the waveform should be transfered in binary
+            (``True``) or ASCII (``False``) formats.
+        :return: The y-component of the waveform.
+        :rtype: `numpy.ndarray`
+        """
+        if self._channel_count > 1:
+            return self.channel[0].data(bin_format=bin_format)
+        else:
+            raise NotImplementedError
