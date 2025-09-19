6ed414cf3e3142fed2453618783a78e9cc182316
Steven Casagrande <steven.casagrande@ibm.com>
2025-09-18 21:38:58 -0400

    Update copyright year

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -49,7 +49,7 @@ master_doc = "index"
 
 # General information about the project.
 project = "InstrumentKit Library"
-copyright = "2013-2022, Steven Casagrande"
+copyright = "2013-2025, Steven Casagrande"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
